3d904887deb4226153e495b83ec599b820f39329
niederhausen <niederhausen@localhost>
2009-01-26 12:10:11 +0100

    started constraint info creation

--- de.topicmapslab.tmcledit.export/src/de/topicmapslab/tmcledit/export/builder/TinyTiMTopicMapBuilder.java
+++ de.topicmapslab.tmcledit.export/src/de/topicmapslab/tmcledit/export/builder/TinyTiMTopicMapBuilder.java
@@ -1,6 +1,11 @@
 package de.topicmapslab.tmcledit.export.builder;
 
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.tinytim.core.Scope;
 import org.tmapi.core.Association;
 import org.tmapi.core.Locator;
 import org.tmapi.core.Topic;
@@ -8,12 +13,23 @@ import org.tmapi.core.TopicMap;
 import org.tmapi.core.TopicMapSystem;
 import org.tmapi.core.TopicMapSystemFactory;
 
+import de.topicmapslab.tmcledit.model.AbstractConstraint;
+import de.topicmapslab.tmcledit.model.AssociationTypeConstraint;
 import de.topicmapslab.tmcledit.model.MappingElement;
+import de.topicmapslab.tmcledit.model.NameTypeConstraint;
+import de.topicmapslab.tmcledit.model.OccurenceTypeConstraint;
+import de.topicmapslab.tmcledit.model.RoleTypeConstraints;
+import de.topicmapslab.tmcledit.model.ScopeConstraint;
+import de.topicmapslab.tmcledit.model.SubjectIdentifierConstraint;
+import de.topicmapslab.tmcledit.model.SubjectLocatorConstraint;
 import de.topicmapslab.tmcledit.model.TopicMapSchema;
 import de.topicmapslab.tmcledit.model.TopicType;
 
 public class TinyTiMTopicMapBuilder {
 	private final TopicMapSchema topicMapSchema;
+	
+	private boolean createConstraintInfos;
+	
 	private Topic scopeType;
 	private Topic nameType;
 	private Topic occurenceType;
@@ -23,7 +39,9 @@ public class TinyTiMTopicMapBuilder {
 	private Topic superTypeRole;
 	private Topic subTypeRole;
 	private Topic supertype_subType_assTopic;
-
+	
+	private Topic infoScope;
+	
 	public TinyTiMTopicMapBuilder(TopicMapSchema topicMapSchema) {
 		this.topicMapSchema = topicMapSchema;
 	}
@@ -34,12 +52,18 @@ public class TinyTiMTopicMapBuilder {
 	
 	public TopicMap getTopicMap(boolean createConstraintInfo) throws Exception{
 		TopicMap tm = null;
-
+		this.createConstraintInfos = createConstraintInfo;
+		
 		TopicMapSystemFactory fac = TopicMapSystemFactory.newInstance();
 		
 		TopicMapSystem system = fac.newTopicMapSystem();
 		
 		tm = system.createTopicMap("http://psi.topicmapslab.de/tmclschema");
+		
+		if (createConstraintInfos) {
+			Locator l = tm.createLocator("#constraint_info_scope");
+			scopeType = tm.createTopicByItemIdentifier(l);
+		}
 		
 		//topicsIndex = (TopicsIndex) tm.getHelperObject(org.tmapi.index.core.TopicsIndex.class);
 
@@ -87,9 +111,56 @@ public class TinyTiMTopicMapBuilder {
 			createTopic(tt, tm);
 		}
 
+		if (createConstraintInfos) {
+			for (AssociationTypeConstraint asc : topicMapSchema.getAssociationTypeConstraints()) {
+				l = tm.createLocator("#topic"+asc.getAssociationType().hashCode());
+				Topic topic = tm.createTopicByItemIdentifier(l); // topic should already exists therefor no new creation
+				
+				StringBuffer buffer = new StringBuffer();
+				
+				buffer.append("Associations of this type should have the following role types: ");
+				for (RoleTypeConstraints rtc : asc.getRoleTypeConstraints()) {
+					buffer.append(rtc.getTopicType().getName());
+					buffer.append(", ");
+				}
+				buffer.setLength(buffer.length()-2);
+				if (asc.getScope().size()>0)
+					buffer.append(" @ scope: ");
+				for (ScopeConstraint sc : asc.getScope()) {
+					buffer.append(sc.getType().getName());
+					buffer.append("[");
+					buffer.append(sc.getCardMin());
+					buffer.append("..");
+					buffer.append(sc.getCardMax());
+					buffer.append("]");
+				}
+				
+				topic.createOccurrence(getConstraintInfoTopic(asc, tm), buffer.toString(), scopeType);
+			}
+		}
 		
 		return tm;
 	}
+	
+	private Topic getConstraintInfoTopic(AbstractConstraint constraint, TopicMap tm) {
+		Locator l = null;
+		if (constraint instanceof AssociationTypeConstraint) {
+			l = tm.createLocator("#association_constraint_info");
+		} else if (constraint instanceof OccurenceTypeConstraint) {
+			l = tm.createLocator("#occurence_constraint_info");
+		} else if (constraint instanceof NameTypeConstraint) {
+			l = tm.createLocator("#name_constraint_info");
+		} else if (constraint  instanceof SubjectIdentifierConstraint) {
+			l = tm.createLocator("#subject_identifier_constraint_info");
+		} else if (constraint instanceof SubjectLocatorConstraint) {
+			l = tm.createLocator("#subject_identifier_constraint_info");
+		} else if (constraint instanceof RoleTypeConstraints) {
+			l = tm.createLocator("#role_constraint_info");
+		}
+		
+		return tm.createTopicByItemIdentifier(l);
+	}
+	
 /*
 	private Association createAssociation(AssociationTypeConstraint atc,
 			TopicMap tm) throws Exception {
