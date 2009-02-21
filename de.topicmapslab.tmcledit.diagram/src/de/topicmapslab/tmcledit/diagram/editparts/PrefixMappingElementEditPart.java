309f5b1d26b7307e662cb0d7e935daa3d5f142f1
niederhausen <niederhausen@localhost>
2009-02-21 17:19:32 +0100

    * modified menu conf in plugin xml  * prefix renders again

--- de.topicmapslab.tmcledit.diagram/src/de/topicmapslab/tmcledit/diagram/editparts/PrefixMappingElementEditPart.java
+++ de.topicmapslab.tmcledit.diagram/src/de/topicmapslab/tmcledit/diagram/editparts/PrefixMappingElementEditPart.java
@@ -10,61 +10,56 @@
  *******************************************************************************/
 package de.topicmapslab.tmcledit.diagram.editparts;
 
-import java.util.Map;
-
+import org.eclipse.draw2d.Figure;
+import org.eclipse.draw2d.IFigure;
 import org.eclipse.draw2d.Label;
-import org.eclipse.draw2d.geometry.Point;
 import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.gef.GraphicalEditPart;
 
-import de.topicmapslab.tmcledit.diagram.policies.PrefixMappingElementEditPolicy;
 import de.topicmapslab.tmcledit.model.MappingElement;
 
-public class PrefixMappingElementEditPart extends AbstractLabelEditPart {
+public class PrefixMappingElementEditPart extends AdapterGraphicalEditPart {
 
-	// helper variable to set the direct edit request extended data map
-	private boolean editingKey;
+	private Label keyLabel;
+	private Label uriLabel;
+	
+	@Override
+	protected IFigure createFigure() {
+		return new Figure();
+	}
+	
+	public Label getKeyLabel() {
+		if (keyLabel == null) {
+			keyLabel = new Label();
+		}
+
+		return keyLabel;
+	}
+	
+	public Label getUriLabel() {
+		if (uriLabel == null) {
+			uriLabel = new Label();
+		}
+
+		return uriLabel;
+	}
 	
 	@Override
 	protected void createEditPolicies() {
-
 	}
-
-	@Override
-	protected Label directEditHitTest(Point requestLoc) {
-		Label result = super.directEditHitTest(requestLoc.getCopy());
-		
-		if (result == null) {
-			getTypeLabel().translateToRelative(requestLoc);
-			if (getTypeLabel().containsPoint(requestLoc)) {
-				result = getTypeLabel();
-				editingKey = false;
-			}
-		} else {
-			editingKey = true;
-		}
-		
-		return result;
-	}
-	
-	@SuppressWarnings("unchecked")
-	@Override
-	protected void fillExtendedData(Map extendedData) {
-		if (editingKey)
-			extendedData.put(PrefixMappingElementEditPolicy.EXT_EDITED,
-					PrefixMappingElementEditPolicy.EXT_EDITED_KEY);
-		else
-			extendedData.put(PrefixMappingElementEditPolicy.EXT_EDITED,
-					PrefixMappingElementEditPolicy.EXT_EDITED_VALUE);
-	}
-	
 	@Override
 	protected void refreshVisuals() {
-		getNameLabel().setText(getCastedModel().getKey());
-		getTypeLabel().setText(getCastedModel().getValue());
+		keyLabel.setText(getCastedModel().getKey());
+		uriLabel.setText(getCastedModel().getValue());
 	}
 	
 	private MappingElement getCastedModel() {
 		return (MappingElement) getModel();
+	}
+	
+	@Override
+	public IFigure getContentPane() {
+		return ((GraphicalEditPart)getParent()).getContentPane();
 	}
 	
 	@Override
