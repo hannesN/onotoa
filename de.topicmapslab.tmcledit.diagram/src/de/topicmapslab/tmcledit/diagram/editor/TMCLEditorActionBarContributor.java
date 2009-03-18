fc922a72ff53cd5111f06b9f4a869ef8e1ca8cca
niederhausen <niederhausen@localhost>
2009-03-18 09:50:01 +0100

    added image export, right now only png are supported

--- de.topicmapslab.tmcledit.diagram/src/de/topicmapslab/tmcledit/diagram/editor/TMCLEditorActionBarContributor.java
+++ de.topicmapslab.tmcledit.diagram/src/de/topicmapslab/tmcledit/diagram/editor/TMCLEditorActionBarContributor.java
@@ -13,36 +13,56 @@
  */
 package de.topicmapslab.tmcledit.diagram.editor;
 
+import java.io.FileOutputStream;
+
+import org.eclipse.draw2d.Graphics;
+import org.eclipse.draw2d.IFigure;
+import org.eclipse.draw2d.SWTGraphics;
+import org.eclipse.draw2d.geometry.Rectangle;
 import org.eclipse.gef.ui.actions.ActionBarContributor;
 import org.eclipse.gef.ui.actions.GEFActionConstants;
 import org.eclipse.gef.ui.actions.ZoomComboContributionItem;
 import org.eclipse.gef.ui.actions.ZoomInRetargetAction;
 import org.eclipse.gef.ui.actions.ZoomOutRetargetAction;
+import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.IToolBarManager;
 import org.eclipse.jface.action.MenuManager;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Device;
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.ImageData;
+import org.eclipse.swt.graphics.ImageLoader;
+import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchActionConstants;
 
 /**
  * @author Hannes Niederhausen
- *
+ * 
  */
 public class TMCLEditorActionBarContributor extends ActionBarContributor {
+
+	ExportAction exportAction = new ExportAction();
+	
 	@Override
 	public void contributeToToolBar(IToolBarManager toolBarManager) {
 		toolBarManager.add(getAction(GEFActionConstants.ZOOM_IN));
 		toolBarManager.add(new ZoomComboContributionItem(getPage()));
 		toolBarManager.add(getAction(GEFActionConstants.ZOOM_OUT));
+		toolBarManager.add(exportAction);
 	}
-	
+
 	@Override
 	public void contributeToMenu(IMenuManager menuManager) {
 		super.contributeToMenu(menuManager);
 
 		MenuManager viewMenu = new MenuManager("&View");
-	    viewMenu.add(getAction(GEFActionConstants.ZOOM_IN));
-	    viewMenu.add(getAction(GEFActionConstants.ZOOM_OUT));
-	    menuManager.insertAfter(IWorkbenchActionConstants.M_EDIT, viewMenu);
+		viewMenu.add(getAction(GEFActionConstants.ZOOM_IN));
+		viewMenu.add(getAction(GEFActionConstants.ZOOM_OUT));
+		viewMenu.add(exportAction);
+		menuManager.insertAfter(IWorkbenchActionConstants.M_EDIT, viewMenu);
 
 	}
 
@@ -54,5 +74,76 @@ public class TMCLEditorActionBarContributor extends ActionBarContributor {
 
 	@Override
 	protected void declareGlobalActionKeys() {
+	}
+
+	@Override
+	public void setActiveEditor(IEditorPart editor) {
+		super.setActiveEditor(editor);
+		if (editor instanceof TMCLDiagramEditor)
+			exportAction.setEditor((TMCLDiagramEditor) editor);
+		else
+			exportAction.setEditor(null);
+	}
+
+	private class ExportAction extends Action {
+		TMCLDiagramEditor editor;
+
+		public ExportAction() {
+			setText("Export diagram");
+		}
+
+		public void setEditor(TMCLDiagramEditor editor) {
+			this.editor = editor;
+		}
+		
+		@Override
+		public void run() {
+			if (editor == null)
+				return;
+
+			FileDialog dlg = new FileDialog(editor.getSite().getShell());
+			dlg.setFilterExtensions(new String[] { "*.png" });
+			String file = dlg.open();
+			if (file == null)
+				return;
+
+			if (!file.endsWith(".png"))
+				file += ".png";
+			
+			
+			IFigure figure = editor.getPrintableFigure();
+			Device device = dlg.getParent().getDisplay();
+			Rectangle r = figure.getBounds();
+			Image image = null;
+			GC gc = null;
+			Graphics g = null;
+			try {
+				FileOutputStream result = new FileOutputStream(file);
+
+				image = new Image(device, r.width, r.height);
+				gc = new GC(image);
+				g = new SWTGraphics(gc);
+				g.translate(r.x * -1, r.y * -1);
+
+				figure.paint(g);
+
+				ImageLoader imageLoader = new ImageLoader();
+				imageLoader.data = new ImageData[] { image.getImageData() };
+				imageLoader.save(result, SWT.IMAGE_PNG);
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			} finally {
+				if (g != null) {
+					g.dispose();
+				}
+				if (gc != null) {
+					gc.dispose();
+				}
+				if (image != null) {
+					image.dispose();
+				}
+			}
+
+		}
 	}
 }
