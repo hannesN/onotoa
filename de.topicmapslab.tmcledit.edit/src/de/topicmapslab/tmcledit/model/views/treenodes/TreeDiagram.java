969a046e1721c620e996be4702aaab5318e4fde8
niederhausen <niederhausen@localhost>
2009-08-19 13:43:31 +0200

    some stuff

--- de.topicmapslab.tmcledit.edit/src/de/topicmapslab/tmcledit/model/views/treenodes/TreeDiagram.java
+++ de.topicmapslab.tmcledit.edit/src/de/topicmapslab/tmcledit/model/views/treenodes/TreeDiagram.java
@@ -14,6 +14,8 @@ import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.jface.dialogs.IInputValidator;
 import org.eclipse.jface.dialogs.InputDialog;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PartInitException;
 
 import de.topicmapslab.tmcledit.model.Diagram;
@@ -29,56 +31,62 @@ public class TreeDiagram extends TreeObject {
 		super(modelView, null);
 		setModel(diagram);
 	}
-	
+
 	@Override
 	public String getName() {
 		return getDiagram().getName();
 	}
-	
+
 	public Diagram getDiagram() {
 		return (Diagram) getModel();
 	}
-	
-	
+
 	@Override
 	public void handleDoubleClick() {
 		try {
-			TmcleditEditPlugin.getPlugin().getWorkbench().getActiveWorkbenchWindow().getActivePage().openEditor(
-			        new TMCLEditorInput(getDiagram(), getModelView().getEditingDomain(), getModelView()
-			                .getActionRegistry(), true), TmcleditEditPlugin.DIAGRAMEDITOR_ID);
+			IWorkbenchPage activePage = TmcleditEditPlugin.getPlugin().getWorkbench().getActiveWorkbenchWindow()
+			        .getActivePage();
+
+			TMCLEditorInput input = new TMCLEditorInput(getDiagram(), getModelView().getEditingDomain(), getModelView()
+			        .getActionRegistry(), true);
+			IEditorPart part = activePage.findEditor(input);
+			if (part == null)
+				activePage.openEditor(input, TmcleditEditPlugin.DIAGRAMEDITOR_ID);
+			else
+				activePage.activate(part);
 		} catch (PartInitException e) {
 			throw new RuntimeException(e);
 		}
 	}
-	
+
 	@Override
 	public Image getImage() {
 		return null;// ImageProvider.getImage(ImageConstants.DIAGRAM);
 	}
-	
+
 	@Override
 	public void handleRename() {
 		String oldName = getDiagram().getName();
-		InputDialog dlg = new InputDialog(getModelView().getViewer().getTree()
-				.getShell(), "New Diagram Name..", "Please enter the new diagram name",
-				oldName, new IInputValidator() {
+		InputDialog dlg = new InputDialog(getModelView().getViewer().getTree().getShell(), "New Diagram Name..",
+		        "Please enter the new diagram name", oldName, new IInputValidator() {
 
-					public String isValid(String newText) {
-						if (newText.length() == 0)
-							return "no name given";
+			        public String isValid(String newText) {
+				        if (newText.length() == 0)
+					        return "no name given";
 
-						return null;
-					}
-				});
+				        return null;
+			        }
+		        });
 		if (InputDialog.OK == dlg.open()) {
 			getModelView().getEditingDomain().getCommandStack().execute(
-					new RenameDiagramCommand(dlg.getValue(), getDiagram()));
+			        new RenameDiagramCommand(dlg.getValue(), getDiagram()));
 		}
 	}
 
 	@Override
 	public void notifyChanged(Notification notification) {
-		if ( (notification.getEventType()==Notification.SET) && (notification.getFeatureID(String.class)==ModelPackage.DIAGRAM__NAME)){
+		if ((notification.getEventType() == Notification.SET)
+		        && (notification.getFeatureID(String.class) == ModelPackage.DIAGRAM__NAME)) {
 			getModelView().getViewer().refresh(this);
 		}
 	}
