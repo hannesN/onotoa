26d3423f7600f88e53ba08bc1a9c85020713a2c3
niederhausen <niederhausen@localhost>
2009-09-16 11:41:13 +0200

    added ExtensionPoint for AnnotationRegistry (#751)

---
+++ de.topicmapslab.tmcledit.edit/src/de/topicmapslab/tmcledit/model/annotationprovider/IAnnotationProposalProvider.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Copyright (c) 2008, 2009 Topic Maps Lab and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Hannes Niederhausen - initial API and implementation
+ *******************************************************************************/
+package de.topicmapslab.tmcledit.model.annotationprovider;
+
+import java.util.Collection;
+
+/**
+ * @author Hannes Niederhausen
+ *
+ */
+public interface IAnnotationProposalProvider {
+	
+	
+	/**
+	 * Tells if the user may enter other values than those returned by getProposals().  
+	 * 
+	 * The default values is <code>true</code>;
+	 * 
+	 * @return <code>true</code> if new values are allowed, <code>false</code> else
+	 */
+	public boolean newValuesAllowed();
+	
+	/**
+	 * Returns a set of valid values for the annotation. This may be used to provide a set of values, 
+	 * where the user may choose from.
+	 * 
+	 * This method must not return <code>null</code>.
+	 *  
+	 * @return A collection containing valid values for the annotation.
+	 */
+	public Collection<Object> getProposals();
+}
