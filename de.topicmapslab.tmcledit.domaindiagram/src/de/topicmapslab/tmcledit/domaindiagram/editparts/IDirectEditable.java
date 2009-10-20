b2eec57d06e8c743ff73b30fbdfe26f1155032f5
niederhausen <niederhausen@localhost>
2009-10-20 19:29:18 +0200

    more context files

---
+++ de.topicmapslab.tmcledit.domaindiagram/src/de/topicmapslab/tmcledit/domaindiagram/editparts/IDirectEditable.java
@@ -0,0 +1,16 @@
+package de.topicmapslab.tmcledit.domaindiagram.editparts;
+
+import org.eclipse.gef.Request;
+import org.eclipse.gef.tools.DirectEditManager;
+
+public interface IDirectEditable {
+
+	public abstract void performRequest(Request req);
+
+	public abstract DirectEditManager getManager();
+
+	public abstract void revertNameChange();
+
+	public abstract void handleNameChange(String value);
+
+}
