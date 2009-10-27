2f6e19ade304cf73207e9328557a9beb9187c68e
niederhausen <niederhausen@localhost>
2009-10-27 16:35:10 +0100

    removed roleconstraint to rpc when changing assoc type

--- de.topicmapslab.tmcledit.domaindiagram/src/de/topicmapslab/tmcledit/domaindiagram/action/SetTypeAction.java
+++ de.topicmapslab.tmcledit.domaindiagram/src/de/topicmapslab/tmcledit/domaindiagram/action/SetTypeAction.java
@@ -6,6 +6,9 @@ package de.topicmapslab.tmcledit.domaindiagram.action;
 import org.eclipse.emf.common.command.AbstractCommand;
 import org.eclipse.emf.common.command.CompoundCommand;
 
+import de.topicmapslab.tmcledit.model.AssociationTypeConstraint;
+import de.topicmapslab.tmcledit.model.ModelPackage;
+import de.topicmapslab.tmcledit.model.RolePlayerConstraint;
 import de.topicmapslab.tmcledit.model.commands.GenericSetCommand;
 
 public class SetTypeAction extends AbstractCommandStackAction {
@@ -19,8 +22,7 @@ public class SetTypeAction extends AbstractCommandStackAction {
 	}
 
 	private void init() {
-		String text = (data.type == null) ? "New Association..." : data.type
-				.getName();
+		String text = (data.type == null) ? "New Type..." : data.type.getName();
 		setText(text);
 	}
 
@@ -28,19 +30,24 @@ public class SetTypeAction extends AbstractCommandStackAction {
 	public void run() {
 		CompoundCommand cmd = new CompoundCommand();
 
-//		AbstractCommand c = null;
-//		if (data.type == null) {
-//			c = createAssociationCommand();
-//			if (c != null)
-//				cmd.append(c);
-//		}
-		cmd.append(getCommand());
+		if (data.typedConstraint instanceof AssociationTypeConstraint) {
+			AssociationTypeConstraint atc = (AssociationTypeConstraint) data.typedConstraint;
 
+			for (RolePlayerConstraint rpc : atc.getPlayerConstraints()) {
+				if (rpc.getRole() != null) {
+					cmd.append(new GenericSetCommand(rpc,
+							ModelPackage.ROLE_PLAYER_CONSTRAINT__ROLE, null));
+				}
+			}
+
+		}
+		cmd.append(getCommand());
 		getCommandStack().execute(cmd);
 	}
-	
+
 	protected AbstractCommand getCommand() {
-		return new GenericSetCommand(data.typedConstraint, data.featureId, data.type);
+		return new GenericSetCommand(data.typedConstraint, data.featureId,
+				data.type);
 	}
 
 }
