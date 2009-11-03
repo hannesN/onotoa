3f4892ec94e817ff1f85d139644d0633ccb48d98
niederhausen <niederhausen@localhost>
2009-11-03 14:25:35 +0100

    fixed #1527

--- de.topicmapslab.tmcledit.domaindiagram/src/de/topicmapslab/tmcledit/domaindiagram/action/AddImageAction.java
+++ de.topicmapslab.tmcledit.domaindiagram/src/de/topicmapslab/tmcledit/domaindiagram/action/AddImageAction.java
@@ -3,12 +3,17 @@
  */
 package de.topicmapslab.tmcledit.domaindiagram.action;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 
 import org.eclipse.emf.common.command.CommandStack;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.swt.widgets.MessageBox;
 
 import de.topicmapslab.tmcledit.domaindiagram.Activator;
 import de.topicmapslab.tmcledit.model.ModelPackage;
@@ -50,6 +55,17 @@ public class AddImageAction extends AbstractCommandStackAction {
 			FileInputStream fis = new FileInputStream(file);
 			byte[] buf = new byte[(int) file.getAbsoluteFile().length()];
 			fis.read(buf);
+			Image img = null;
+			try {
+				img = new Image(Display.getCurrent(), new ByteArrayInputStream(buf));
+			} catch (Exception imgException) {
+				MessageDialog.openError(Activator.getCurrentShell(), "Invalid file", "Invalid file type");
+				return;
+			} finally {
+				if ( (img!=null) && (!img.isDisposed()) ) {
+					img.dispose();
+				}
+			}
 			
 			GenericSetCommand command = new GenericSetCommand(typeNode, ModelPackage.TYPE_NODE__IMAGE, Base64.byteArrayToBase64(buf));
 			getCommandStack().execute(command);
