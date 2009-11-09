50e5b4a94f7ef66dd7333738c4d5eac8a3cbb003
niederhausen <niederhausen@localhost>
2009-11-09 21:27:56 +0100

    fixed: missing types in domain diagram

--- de.topicmapslab.tmcledit.domaindiagram/src/de/topicmapslab/tmcledit/domaindiagram/editor/DomainEditorContextMenuProvider.java
+++ de.topicmapslab.tmcledit.domaindiagram/src/de/topicmapslab/tmcledit/domaindiagram/editor/DomainEditorContextMenuProvider.java
@@ -10,7 +10,6 @@
  *******************************************************************************/
 package de.topicmapslab.tmcledit.domaindiagram.editor;
 
-
 import java.util.Iterator;
 
 import org.eclipse.gef.ContextMenuProvider;
@@ -41,16 +40,17 @@ public class DomainEditorContextMenuProvider extends ContextMenuProvider {
 	private final ActionRegistry actionRegistry;
 	private boolean active;
 	private final Diagram diagram;
-	
+
 	private EditPart selectedEditPart;
-	
-	public DomainEditorContextMenuProvider(EditPartViewer viewer, ActionRegistry actionRegistry, Diagram diagram) {
+
+	public DomainEditorContextMenuProvider(EditPartViewer viewer,
+			ActionRegistry actionRegistry, Diagram diagram) {
 		super(viewer);
 		this.actionRegistry = actionRegistry;
 		active = true;
 		this.diagram = diagram;
 	}
-	
+
 	public void setActive(boolean active) {
 		this.active = active;
 	}
@@ -58,104 +58,114 @@ public class DomainEditorContextMenuProvider extends ContextMenuProvider {
 	public void setSelectedEditPart(EditPart selectedEditPart) {
 		this.selectedEditPart = selectedEditPart;
 	}
-	
+
 	@Override
 	public void buildContextMenu(IMenuManager menu) {
 		if (!active)
 			return;
 		GEFActionConstants.addStandardActionGroups(menu);
-		
+
 		IAction action;
 		action = getActionRegistry().getAction(ActionFactory.UNDO.getId());
 		menu.appendToGroup(GEFActionConstants.GROUP_UNDO, action);
 
 		action = getActionRegistry().getAction(ActionFactory.REDO.getId());
 		menu.appendToGroup(GEFActionConstants.GROUP_UNDO, action);
-		
+
 		action = getActionRegistry().getAction(AddNameConstraintAction.ID);
 		if (action.isEnabled())
 			menu.appendToGroup(GEFActionConstants.GROUP_EDIT, action);
-		
+
 		action = getActionRegistry().getAction(AddOccurrenceConstraintAction.ID);
-			if (action.isEnabled())
-				menu.appendToGroup(GEFActionConstants.GROUP_EDIT, action);
-			
+		if (action.isEnabled())
+			menu.appendToGroup(GEFActionConstants.GROUP_EDIT, action);
+
 		action = getActionRegistry().getAction(RemoveFromDiagramAction.ID);
 		if (action.isEnabled())
 			menu.appendToGroup(GEFActionConstants.GROUP_EDIT, action);
-		
+
 		action = getActionRegistry().getAction(DeleteFromModelAction.ID);
 		if (action.isEnabled())
 			menu.appendToGroup(GEFActionConstants.GROUP_EDIT, action);
 
 		buildMoveToDiagramActions(menu);
 		buildCopyToDiagramActions(menu);
-		
-		if ( (selectedEditPart !=null) && (selectedEditPart instanceof IContextMenuProvider) ) {
-			for (IAction a : ((IContextMenuProvider) selectedEditPart).getActions()) {
+
+		if ((selectedEditPart != null)
+				&& (selectedEditPart instanceof IContextMenuProvider)) {
+			for (IAction a : ((IContextMenuProvider) selectedEditPart)
+					.getActions()) {
 				if (a.isEnabled())
 					menu.add(a);
 			}
-			for (IContributionItem i : ((IContextMenuProvider) selectedEditPart).getItems()) {
+			for (IContributionItem i : ((IContextMenuProvider) selectedEditPart)
+					.getItems()) {
 				menu.add(i);
 			}
 		}
-		
+
 	}
-	
+
 	private void buildMoveToDiagramActions(IMenuManager menu) {
 		File file = (File) diagram.eContainer();
-		if ( (file.getDiagrams().size()>1) && (mayMove()) ){
+		if ((file.getDiagrams().size() > 1) && (mayMove())) {
 			MenuManager moveMenu = new MenuManager("&Move To...");
-			
+
 			for (Diagram d : file.getDiagrams()) {
 				if (!d.equals(diagram)) {
-					org.eclipse.emf.common.command.CommandStack commandStack = ((DomainEditDomain)getViewer().getEditDomain()).getEditingDomain().getCommandStack();
-					MoveToDiagramAction a = new MoveToDiagramAction(commandStack, d, getViewer());
+					org.eclipse.emf.common.command.CommandStack commandStack = ((DomainEditDomain) getViewer()
+							.getEditDomain()).getEditingDomain()
+							.getCommandStack();
+					MoveToDiagramAction a = new MoveToDiagramAction(
+							commandStack, d, getViewer());
 					moveMenu.add(a);
 				}
 			}
 			menu.add(moveMenu);
-			
+
 		}
 	}
-	
+
 	private void buildCopyToDiagramActions(IMenuManager menu) {
 		File file = (File) diagram.eContainer();
-		if ( (file.getDiagrams().size()>1) && (mayMove()) ){
+		if ((file.getDiagrams().size() > 1) && (mayMove())) {
 			MenuManager moveMenu = new MenuManager("&Copy To...");
-			
+
 			for (Diagram d : file.getDiagrams()) {
 				if (!d.equals(diagram)) {
-					org.eclipse.emf.common.command.CommandStack commandStack = ((DomainEditDomain)getViewer().getEditDomain()).getEditingDomain().getCommandStack();
-					CopyToDiagramAction a2 = new CopyToDiagramAction(commandStack, d, getViewer());
+					org.eclipse.emf.common.command.CommandStack commandStack = ((DomainEditDomain) getViewer()
+							.getEditDomain()).getEditingDomain()
+							.getCommandStack();
+					CopyToDiagramAction a2 = new CopyToDiagramAction(
+							commandStack, d, getViewer());
 					moveMenu.add(a2);
 				}
 			}
 			menu.add(moveMenu);
-			
+
 		}
 	}
+
 	@SuppressWarnings("unchecked")
 	private boolean mayMove() {
-		IStructuredSelection sel = (IStructuredSelection) getViewer().getSelection();
+		IStructuredSelection sel = (IStructuredSelection) getViewer()
+				.getSelection();
 		if (sel.isEmpty())
 			return false;
-		
+
 		Iterator it = sel.iterator();
 		while (it.hasNext()) {
 			Object next = it.next();
 			if (next instanceof EditPart) {
-			if (((EditPart)next).getModel() instanceof Node)
-				return true;
+				if (((EditPart) next).getModel() instanceof Node)
+					return true;
 			}
 		}
-		
-		
+
 		return false;
-		
+
 	}
-	
+
 	public ActionRegistry getActionRegistry() {
 		return actionRegistry;
 	}
