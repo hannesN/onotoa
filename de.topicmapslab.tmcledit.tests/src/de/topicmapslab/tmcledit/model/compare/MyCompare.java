b3ab73a2f99bf223be7b6ca303a94a5743844cc2
lippert <lippert@localhost>
2010-03-23 19:02:44 +0100

    Update TopicTypeTest and startet to write the TopicTypeComparator

--- de.topicmapslab.tmcledit.tests/src/de/topicmapslab/tmcledit/model/compare/MyCompare.java
+++ de.topicmapslab.tmcledit.tests/src/de/topicmapslab/tmcledit/model/compare/MyCompare.java
@@ -2,6 +2,14 @@ package de.topicmapslab.tmcledit.model.compare;
 
 public class MyCompare {
 
+	/**
+	 * Boolean compare of two integer values 
+	 * 
+	 * @param one int #1
+	 * @param two int #1
+	 * @return boolean result of the compare
+	 */
+	
 	public static boolean intCompare(int one, int two) {
 
 		if (one != two)
@@ -10,6 +18,15 @@ public class MyCompare {
 		return true;
 	}
 
+	/**
+	 * Check if two Strings are the same with the special condition,
+	 * that two Null objects are equal.
+	 * 
+	 * @param one String #1
+	 * @param two String #2
+	 * @return boolean result of the compare
+	 */
+	
 	public static boolean stringCompare(String one, String two) {
 
 		if ((one == null) ^ (two == null))
