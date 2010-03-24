90c0d9248c6d396db35889391ce1d6d4a9489220
lippert <lippert@localhost>
2010-03-24 11:38:36 +0100

    Implemented OccurrenceTypeConstraintTest

---
+++ de.topicmapslab.tmcledit.tests/src/de/topicmapslab/tmcledit/model/compare/AbstractTypedCardinalityConstraintComparator.java
@@ -0,0 +1,23 @@
+/**
+ * (C) 2008 Hannes Niederhausen, Topic Maps Lab
+ *
+ * $Id$
+ */
+package de.topicmapslab.tmcledit.model.compare;
+
+import de.topicmapslab.tmcledit.model.AbstractTypedCardinalityConstraint;
+import de.topicmapslab.tmcledit.model.ModelFactory;
+import de.topicmapslab.tmcledit.model.tests.AbstractCardinalityContraintTest;
+
+import junit.textui.TestRunner;
+
+/**
+ * <!-- begin-user-doc -->
+ * A test case for the model object '<em><b>Abstract Typed Cardinality Constraint</b></em>'.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class AbstractTypedCardinalityConstraintComparator extends AbstractCardinalityConstraintComparator {
+
+
+} //AbstractTypedCardinalityConstraintTest
---
+++ de.topicmapslab.tmcledit.tests/src/de/topicmapslab/tmcledit/model/compare/OccurrenceTypeConstraintComparator.java
@@ -0,0 +1,23 @@
+/**
+ * (C) 2008 Hannes Niederhausen, Topic Maps Lab
+ *
+ * $Id$
+ */
+package de.topicmapslab.tmcledit.model.compare;
+
+import de.topicmapslab.tmcledit.model.ModelFactory;
+import de.topicmapslab.tmcledit.model.OccurrenceTypeConstraint;
+import de.topicmapslab.tmcledit.model.tests.AbstractTypedCardinalityConstraintTest;
+
+import junit.textui.TestRunner;
+
+/**
+ * <!-- begin-user-doc -->
+ * A test case for the model object '<em><b>Occurrence Type Constraint</b></em>'.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class OccurrenceTypeConstraintComparator extends AbstractTypedCardinalityConstraintComparator{
+
+	
+} //OccurrenceTypeConstraintTest
