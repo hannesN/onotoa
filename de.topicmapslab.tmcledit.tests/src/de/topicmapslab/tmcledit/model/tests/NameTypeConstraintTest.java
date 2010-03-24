02722d90e280edc24280e788f46fc7e80953022f
lippert <lippert@localhost>
2010-03-24 16:20:20 +0100

    implemented NameTypeConstraintTest and NameTypeConstraintComparator

--- de.topicmapslab.tmcledit.tests/src/de/topicmapslab/tmcledit/model/tests/NameTypeConstraintTest.java
+++ de.topicmapslab.tmcledit.tests/src/de/topicmapslab/tmcledit/model/tests/NameTypeConstraintTest.java
@@ -5,69 +5,222 @@
  */
 package de.topicmapslab.tmcledit.model.tests;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import de.topicmapslab.tmcledit.model.Annotation;
 import de.topicmapslab.tmcledit.model.ModelFactory;
 import de.topicmapslab.tmcledit.model.NameTypeConstraint;
+import de.topicmapslab.tmcledit.model.TopicId;
+import de.topicmapslab.tmcledit.model.TopicType;
+import de.topicmapslab.tmcledit.model.compare.NameTypeConstraintComparator;
 
-import junit.textui.TestRunner;
+import junit.framework.Assert;
 
-/**
- * <!-- begin-user-doc -->
- * A test case for the model object '<em><b>Name Type Constraint</b></em>'.
- * <!-- end-user-doc -->
- * @generated
- */
-public class NameTypeConstraintTest extends AbstractTypedCardinalityConstraintTest {
+public class NameTypeConstraintTest {
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	public static void main(String[] args) {
-		TestRunner.run(NameTypeConstraintTest.class);
+	private NameTypeConstraint testObject1;
+	private NameTypeConstraint testObject2;
+	private NameTypeConstraintComparator comp;
+
+	@Before
+	public void prepare() {
+
+		if (testObject1 == null)
+			testObject1 = ModelFactory.eINSTANCE.createNameTypeConstraint();
+		if (testObject2 == null)
+			testObject2 = ModelFactory.eINSTANCE.createNameTypeConstraint();
+
+		comp = new NameTypeConstraintComparator();
+	}
+
+	@After
+	public void shutdown() {
+		testObject1 = null;
+		testObject2 = null;
 	}
 
 	/**
-	 * Constructs a new Name Type Constraint test case with the given name.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
+	 * Test compares Object with NULL and proves the equality of two NULL
+	 * Objects
 	 */
-	public NameTypeConstraintTest(String name) {
-		super(name);
+
+	@Test
+	public void nullTest() {
+
+		Assert.assertFalse(comp.equals(testObject1, null));
+		Assert.assertFalse(comp.equals(null, testObject1));
+		Assert.assertTrue(comp.equals(null, null));
 	}
 
 	/**
-	 * Returns the fixture for this Name Type Constraint test case.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
+	 * Test compares two objects with different and same IDs.
 	 */
-	@Override
-	protected NameTypeConstraint getFixture() {
-		return (NameTypeConstraint)fixture;
+
+	@Test
+	public void idTest() {
+
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		testObject1.setId(testObject2.getId());
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @see junit.framework.TestCase#setUp()
-	 * @generated
+	 * Test compares two objects with different and equal see_Also values.
 	 */
-	@Override
-	protected void setUp() throws Exception {
-		setFixture(ModelFactory.eINSTANCE.createNameTypeConstraint());
+
+	@Test
+	public void seeAlsoTest() {
+
+		testObject1.setId(testObject2.getId());
+
+		testObject1.setSee_also("TMCL");
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		testObject2.setSee_also("TMCL");
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @see junit.framework.TestCase#tearDown()
-	 * @generated
+	 * Test compares two objects with different and same descriptions.
 	 */
-	@Override
-	protected void tearDown() throws Exception {
-		setFixture(null);
+
+	@Test
+	public void descriptionTest() {
+
+		testObject1.setId(testObject2.getId());
+
+		testObject1.setDescription("TMCL");
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		testObject2.setDescription("TMCL");
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
 	}
 
-} //NameTypeConstraintTest
+	/**
+	 * Test compares two objects with different and same comments.
+	 */
+
+	@Test
+	public void commentTest() {
+
+		testObject1.setId(testObject2.getId());
+
+		testObject1.setComment("TMCL");
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		testObject2.setComment("TMCL");
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+	}
+
+	/**
+	 * Test compares objects with different and equal annotations.
+	 */
+
+	@Test
+	public void annotationTest() {
+
+		testObject1.setId(testObject2.getId());
+		Annotation annotation1 = ModelFactory.eINSTANCE.createAnnotation();
+		Annotation annotation2 = ModelFactory.eINSTANCE.createAnnotation();
+
+		// add annotation to testObject1
+		annotation1.setKey("TMCL");
+		annotation1.setValue("TMCL");
+		testObject1.getAnnotations().add(annotation1);
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+
+		// add a different annotation to testObject2
+		annotation2.setId(annotation1.getId());
+		annotation2.setKey("TMC");
+		annotation2.setValue("TMC");
+		testObject2.getAnnotations().add(annotation2);
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+
+		// make both annotations the same
+		annotation2.setKey("TMCL");
+		annotation2.setValue("TMCL");
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+	}
+
+	/**
+	 * Test compares two objects with different and same cardMin values. Default
+	 * = 0
+	 */
+
+	@Test
+	public void cardMinTest() {
+
+		testObject1.setId(testObject2.getId());
+
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+		testObject1.setCardMin("1");
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		testObject2.setCardMin("1");
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+
+	}
+
+	/**
+	 * Test compares two objects with different and same cardMax values. Default
+	 * = *
+	 */
+
+	@Test
+	public void cardMaxTest() {
+
+		testObject1.setId(testObject2.getId());
+
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+		testObject1.setCardMax("2");
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		testObject2.setCardMax("2");
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+
+	}
+
+	/**
+	 * Test compares two objects with different and same topic types
+	 */
+
+	@Test
+	public void topicTypetest() {
+
+		TopicType topicType1 = ModelFactory.eINSTANCE.createTopicType();
+		TopicType topicType2 = ModelFactory.eINSTANCE.createTopicType();
+		testObject1.setId(testObject2.getId());
+
+		// set different topic types
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+		testObject1.setType(topicType1);
+		testObject2.setType(topicType2);
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+
+		// make both topic types the same
+		topicType1.setId(topicType2.getId());
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+
+		// change some values of the topic types
+		topicType1.setAbstract(true);
+		topicType2.setAbstract(false);
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		topicType2.setAbstract(true);
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+
+		topicType1.setName("TMCL");
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		topicType2.setName("TMQL");
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		topicType2.setName("TMCL");
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+
+		TopicId topicId1 = TopicId.SUBJECT_IDENTIFIER;
+		TopicId topicId2 = TopicId.ITEM_IDENTIFIER;
+
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+		topicType1.setIdType(topicId1);
+		topicType2.setIdType(topicId2);
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		topicType1.setIdType(topicId2);
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+
+	}
+} // NameTypeConstraintTest
