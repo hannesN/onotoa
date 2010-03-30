37a7ad22564339e5d949a9ac0cc515aa6aaed35a
lippert <lippert@localhost>
2010-03-30 10:52:00 +0200

    updated MappingElementTest to junit 4

--- de.topicmapslab.tmcledit.tests/src/de/topicmapslab/tmcledit/model/compare/MappingElementComparator.java
+++ de.topicmapslab.tmcledit.tests/src/de/topicmapslab/tmcledit/model/compare/MappingElementComparator.java
@@ -6,20 +6,11 @@
 package de.topicmapslab.tmcledit.model.compare;
 
 import de.topicmapslab.tmcledit.model.MappingElement;
-import de.topicmapslab.tmcledit.model.ModelFactory;
 
-import junit.textui.TestRunner;
-
-/**
- * <!-- begin-user-doc --> A test case for the model object '
- * <em><b>Mapping Element</b></em>'. <!-- end-user-doc -->
- * 
- * @generated
- */
 public class MappingElementComparator extends OnoObjectComparator {
 
 	public boolean equals(MappingElement o1, MappingElement o2) {
-		
+
 		if ((o1 == null) ^ (o2 == null))
 			return false;
 
@@ -28,13 +19,13 @@ public class MappingElementComparator extends OnoObjectComparator {
 
 		if (super.equals(o1, o2) == false)
 			return false;
-		
+
 		if (stringCompare(o1.getKey(), o2.getKey()) == false)
 			return false;
-		
+
 		if (stringCompare(o1.getValue(), o2.getValue()) == false)
 			return false;
-		
+
 		return true;
 	}
 
