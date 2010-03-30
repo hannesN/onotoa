83d80cf7e113bc3b105862529a6516df8ec03c10
lippert <lippert@localhost>
2010-03-30 16:04:36 +0200

    FileComparator complete implemented

--- de.topicmapslab.tmcledit.tests/src/de/topicmapslab/tmcledit/model/compare/FileComparator.java
+++ de.topicmapslab.tmcledit.tests/src/de/topicmapslab/tmcledit/model/compare/FileComparator.java
@@ -5,6 +5,9 @@
  */
 package de.topicmapslab.tmcledit.model.compare;
 
+import org.eclipse.emf.common.util.EList;
+
+import de.topicmapslab.tmcledit.model.Diagram;
 import de.topicmapslab.tmcledit.model.File;
 
 public class FileComparator extends OnoObjectComparator {
@@ -25,9 +28,40 @@ public class FileComparator extends OnoObjectComparator {
 
 		if (o1.isDirty() != o2.isDirty())
 			return false;
-		
+
+		TopicMapSchemaComparatror comp = new TopicMapSchemaComparatror();
+
+		if (comp.equals(o1.getTopicMapSchema(), o2.getTopicMapSchema()) == false)
+			return false;
+
+		if (o1.getDiagrams().size() == o2.getDiagrams().size()) {
+			if ((diagramListCompare(o1.getDiagrams(), o2.getDiagrams())) == false)
+				return false;
+		} else
+			return false;
+
 		return true;
 
 	}
 
+	/**
+	 * Compares two lists with Diagrams
+	 * 
+	 * @param list1
+	 * @param list2
+	 * @return boolean result of the compare
+	 */
+
+	private boolean diagramListCompare(EList<Diagram> list1,
+			EList<Diagram> list2) {
+
+		DiagramComparator comp = new DiagramComparator();
+
+		for (int i = 0; i < list1.size(); i++) {
+			if (comp.equals(list1.get(i), list2.get(i)) == false)
+				return false;
+		}
+
+		return true;
+	}
 } // FileTest
