b140d7d7633552e37a6b02a54ddf8eb3de93309c
lippert <lippert@localhost>
2010-03-30 10:58:04 +0200

    updated NodeTest to junit 4

--- de.topicmapslab.tmcledit.tests/src/de/topicmapslab/tmcledit/model/tests/NodeTest.java
+++ de.topicmapslab.tmcledit.tests/src/de/topicmapslab/tmcledit/model/tests/NodeTest.java
@@ -5,127 +5,96 @@
  */
 package de.topicmapslab.tmcledit.model.tests;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 import de.topicmapslab.tmcledit.model.ModelFactory;
 import de.topicmapslab.tmcledit.model.Node;
 import de.topicmapslab.tmcledit.model.compare.NodeComparator;
 
 import junit.framework.Assert;
-import junit.textui.TestRunner;
 
-/**
- * <!-- begin-user-doc --> A test case for the model object '
- * <em><b>Node</b></em>'. <!-- end-user-doc -->
- * 
- * @generated
- */
 public class NodeTest extends OnoObjectTest {
 
-	/**
-	 * <!-- begin-user-doc --> <!-- end-user-doc -->
-	 * 
-	 * @generated
-	 */
-	public static void main(String[] args) {
-		TestRunner.run(NodeTest.class);
+	private Node testObject1;
+	private Node testObject2;
+	private NodeComparator comp;
+
+	@Before
+	public void prepare() {
+
+		if (testObject1 == null)
+			testObject1 = ModelFactory.eINSTANCE.createNode();
+		if (testObject2 == null)
+			testObject2 = ModelFactory.eINSTANCE.createNode();
+
+		comp = new NodeComparator();
+	}
+
+	@After
+	public void shutdown() {
+		testObject1 = null;
+		testObject2 = null;
 	}
 
 	/**
-	 * Constructs a new Node test case with the given name. <!-- begin-user-doc
-	 * --> <!-- end-user-doc -->
-	 * 
-	 * @generated
+	 * Test compares Object with NULL and proves the equality of two NULL
+	 * Objects
 	 */
-	public NodeTest(String name) {
-		super(name);
+
+	@Test
+	public void nullTest() {
+
+		Assert.assertFalse(comp.equals(testObject1, null));
+		Assert.assertFalse(comp.equals(null, testObject1));
+		Assert.assertTrue(comp.equals(null, null));
 	}
 
 	/**
-	 * Returns the fixture for this Node test case. <!-- begin-user-doc --> <!--
-	 * end-user-doc -->
-	 * 
-	 * @generated
+	 * Test compares two objects with different and same IDs.
 	 */
-	@Override
-	protected Node getFixture() {
-		return (Node) fixture;
+
+	@Test
+	public void idTest() {
+
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		testObject1.setId(testObject2.getId());
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
 	}
 
 	/**
-	 * <!-- begin-user-doc --> <!-- end-user-doc -->
-	 * 
-	 * @see junit.framework.TestCase#setUp()
-	 * @generated
-	 */
-	@Override
-	protected void setUp() throws Exception {
-		setFixture(ModelFactory.eINSTANCE.createNode());
-	}
-
-	/**
-	 * <!-- begin-user-doc --> <!-- end-user-doc -->
-	 * 
-	 * @see junit.framework.TestCase#tearDown()
-	 * @generated
-	 */
-	@Override
-	protected void tearDown() throws Exception {
-		setFixture(null);
-	}
-
-	public void testNode() {
-
-		Node testObject1 = ModelFactory.eINSTANCE.createNode();
-		Node testObject2 = ModelFactory.eINSTANCE.createNode();
-
-		NodeComparator comp = new NodeComparator();
-
-		this.allTests(testObject1, testObject2, comp);
-
-	}
-
-	/**
-	 * Includes super class tests and posX and posY test.
-	 * 
-	 * @param testObject1
-	 * @param testObject2
-	 * @param comp
+	 * Test compares two objects with different and same vales for the x
+	 * position. DEFAULT = 0
 	 */
 
-	protected void allTests(Node testObject1, Node testObject2,
-			NodeComparator comp) {
+	@Test
+	public void posXTEst() {
 
-		super.allTests(testObject1, testObject2, comp);
+		testObject1.setId(testObject2.getId());
 
-		// position test for X and Y value
-		positionTest(testObject1, testObject2, comp);
-	}
-
-	/**
-	 * Method inserts values for the X and Y position which are different from
-	 * the default ones for assertion tests.
-	 * 
-	 * @param testObject1
-	 *            Node #1
-	 * @param testObject2
-	 *            Node #2
-	 * @param comp
-	 *            specific comparator
-	 */
-
-	protected void positionTest(Node testObject1, Node testObject2,
-			NodeComparator comp) {
-
-		// set posX DEFAULT = 0
 		testObject1.setPosX(1);
 		Assert.assertFalse(comp.equals(testObject1, testObject2));
 		testObject2.setPosX(1);
 		Assert.assertTrue(comp.equals(testObject1, testObject2));
 
-		// set posY DEFAULT = 0
+	}
+
+	/**
+	 * Test compares two objects with different and same vales for the y
+	 * position. DEFAULT = 0
+	 */
+
+	@Test
+	public void posYTest() {
+
+		testObject1.setId(testObject2.getId());
+
 		testObject1.setPosY(1);
 		Assert.assertFalse(comp.equals(testObject1, testObject2));
 		testObject2.setPosY(1);
 		Assert.assertTrue(comp.equals(testObject1, testObject2));
+
 	}
 
 } // NodeTest
