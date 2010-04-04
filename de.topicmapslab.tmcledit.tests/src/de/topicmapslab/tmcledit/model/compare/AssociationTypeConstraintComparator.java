59e14e0834f6b9c9741092924600bb1f00c0680b
lippert <lippert@localhost>
2010-04-04 12:44:12 +0200

    Implemented AssociationTypeConstraintComparator

--- de.topicmapslab.tmcledit.tests/src/de/topicmapslab/tmcledit/model/compare/AssociationTypeConstraintComparator.java
+++ de.topicmapslab.tmcledit.tests/src/de/topicmapslab/tmcledit/model/compare/AssociationTypeConstraintComparator.java
@@ -5,7 +5,10 @@
  */
 package de.topicmapslab.tmcledit.model.compare;
 
+import org.eclipse.emf.common.util.EList;
+
 import de.topicmapslab.tmcledit.model.AssociationTypeConstraint;
+import de.topicmapslab.tmcledit.model.RolePlayerConstraint;
 
 public class AssociationTypeConstraintComparator extends
 		AbstractTypedConstraintComparator {
@@ -22,8 +25,36 @@ public class AssociationTypeConstraintComparator extends
 		if (super.equals(o1, o2) == false)
 			return false;
 
+		TopicTypeComparator comp = new TopicTypeComparator();
+
+		if (comp.equals(o1.getType(), o2.getType()) == false)
+			return false;
+
+		if (o1.getPlayerConstraints().size() == o2.getPlayerConstraints()
+				.size()) {
+			if (rolePlayerConstraintListCompare(o1.getPlayerConstraints(), o2
+					.getPlayerConstraints()) == false)
+				return false;
+		} else
+			return false;
+
 		return true;
 
+	}
+
+	protected boolean rolePlayerConstraintListCompare(
+			EList<RolePlayerConstraint> list1, EList<RolePlayerConstraint> list2) {
+
+		RolePlayerConstraintComparator comp = new RolePlayerConstraintComparator();
+
+		for (int i = 0; i < list1.size(); i++) {
+
+			if (comp.equals(list1.get(i), list2.get(i)) == false)
+				return false;
+
+		}
+
+		return true;
 	}
 
 } // AssociationTypeConstraintTest
