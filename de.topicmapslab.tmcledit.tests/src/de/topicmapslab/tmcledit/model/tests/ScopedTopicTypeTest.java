96f8127989774c73499131a1dea5aeeacee34fe7
lippert <lippert@localhost>
2010-04-07 16:06:00 +0200

    Implemented ScopeConstraintTest and some stubs

--- de.topicmapslab.tmcledit.tests/src/de/topicmapslab/tmcledit/model/tests/ScopedTopicTypeTest.java
+++ de.topicmapslab.tmcledit.tests/src/de/topicmapslab/tmcledit/model/tests/ScopedTopicTypeTest.java
@@ -5,35 +5,8 @@
  */
 package de.topicmapslab.tmcledit.model.tests;
 
-import de.topicmapslab.tmcledit.model.ScopedTopicType;
 
-/**
- * <!-- begin-user-doc -->
- * A test case for the model object '<em><b>Scoped Topic Type</b></em>'.
- * <!-- end-user-doc -->
- * @generated
- */
 public abstract class ScopedTopicTypeTest extends TopicTypeTest {
 
-	/**
-	 * Constructs a new Scoped Topic Type test case with the given name.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	public ScopedTopicTypeTest(String name) {
-		super(name);
-	}
-
-	/**
-	 * Returns the fixture for this Scoped Topic Type test case.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	@Override
-	protected ScopedTopicType getFixture() {
-		return (ScopedTopicType)fixture;
-	}
 
 } //ScopedTopicTypeTest
