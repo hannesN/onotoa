550e48fb8dc059046683f1bb8e123d784feb5b45
lippert <lippert@localhost>
2010-04-07 20:08:18 +0200

    Implemented DomainDiagramComparator

--- de.topicmapslab.tmcledit.tests/src/de/topicmapslab/tmcledit/model/compare/DomainDiagramComparator.java
+++ de.topicmapslab.tmcledit.tests/src/de/topicmapslab/tmcledit/model/compare/DomainDiagramComparator.java
@@ -11,6 +11,15 @@ public class DomainDiagramComparator extends DiagramComparator {
 
 	public boolean equals(DomainDiagram o1, DomainDiagram o2) {
 
+		if ((o1 == null) ^ (o2 == null))
+			return false;
+
+		if ((o1 == null) && (o2 == null))
+			return true;
+
+		if (super.equals(o1, o2) == false)
+			return false;
+
 		return true;
 
 	}
