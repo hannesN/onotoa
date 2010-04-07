f444339279730c44014f7de8b184b64e28ada0d9
lippert <lippert@localhost>
2010-04-07 18:01:48 +0200

    Implemented ReifierConstraintTest

---
+++ de.topicmapslab.tmcledit.tests/src/de/topicmapslab/tmcledit/model/compare/ReifierConstraintComparator.java
@@ -0,0 +1,27 @@
+/**
+ * (C) 2008 Hannes Niederhausen, Topic Maps Lab
+ *
+ * $Id$
+ */
+package de.topicmapslab.tmcledit.model.compare;
+
+import de.topicmapslab.tmcledit.model.ReifierConstraint;
+
+public class ReifierConstraintComparator extends
+		AbstractTypedCardinalityConstraintComparator {
+
+	public boolean equals(ReifierConstraint o1, ReifierConstraint o2) {
+
+		if ((o1 == null) ^ (o2 == null))
+			return false;
+
+		if ((o1 == null) && (o2 == null))
+			return true;
+
+		if (super.equals(o1, o2) == false)
+			return false;
+
+		return true;
+
+	}
+} // ReifierConstraintTest
