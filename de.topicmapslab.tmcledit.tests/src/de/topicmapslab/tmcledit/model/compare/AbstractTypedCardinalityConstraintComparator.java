ddf4e3d2f8eba8741e221bac1cd99a9469224f11
lippert <lippert@localhost>
2010-04-07 15:13:21 +0200

    Implemented DiagrammTest

--- de.topicmapslab.tmcledit.tests/src/de/topicmapslab/tmcledit/model/compare/AbstractTypedCardinalityConstraintComparator.java
+++ de.topicmapslab.tmcledit.tests/src/de/topicmapslab/tmcledit/model/compare/AbstractTypedCardinalityConstraintComparator.java
@@ -6,17 +6,7 @@
 package de.topicmapslab.tmcledit.model.compare;
 
 import de.topicmapslab.tmcledit.model.AbstractTypedCardinalityConstraint;
-import de.topicmapslab.tmcledit.model.ModelFactory;
-import de.topicmapslab.tmcledit.model.tests.AbstractCardinalityConstraintTest;
 
-import junit.textui.TestRunner;
-
-/**
- * <!-- begin-user-doc --> A test case for the model object '
- * <em><b>Abstract Typed Cardinality Constraint</b></em>'. <!-- end-user-doc -->
- * 
- * @generated
- */
 public class AbstractTypedCardinalityConstraintComparator extends
 		AbstractCardinalityConstraintComparator {
 
@@ -36,7 +26,7 @@ public class AbstractTypedCardinalityConstraintComparator extends
 
 		if (comp.equals(o1.getType(), o2.getType()) == false)
 			return false;
-		
+
 		return true;
 
 	}
