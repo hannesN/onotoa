06124064fbae2cece2721e8f97701b5942a33551
lippert <lippert@localhost>
2010-04-07 19:44:42 +0200

    Implemented ReifiableTopicTypeTest

---
+++ de.topicmapslab.tmcledit.tests/src/de/topicmapslab/tmcledit/model/compare/ReifiableTopicTypeComparator.java
@@ -0,0 +1,27 @@
+/**
+ * (C) 2008 Hannes Niederhausen, Topic Maps Lab
+ *
+ * $Id$
+ */
+package de.topicmapslab.tmcledit.model.compare;
+
+import de.topicmapslab.tmcledit.model.ReifiableTopicType;
+
+public class ReifiableTopicTypeComparator extends TopicTypeComparator {
+
+	public boolean equals(ReifiableTopicType o1, ReifiableTopicType o2) {
+
+		if ((o1 == null) ^ (o2 == null))
+			return false;
+
+		if ((o1 == null) && (o2 == null))
+			return true;
+
+		if (super.equals(o1, o2) == false)
+			return false;
+
+		return false;
+
+	}
+
+} // ReifiableTopicTypeTest
