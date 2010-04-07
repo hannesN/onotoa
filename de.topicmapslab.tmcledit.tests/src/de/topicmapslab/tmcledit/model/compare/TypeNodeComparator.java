4ffb7ce7564a263d6b7cb14bd94920f0419f5945
lippert <lippert@localhost>
2010-04-07 16:24:49 +0200

    Implemented TypeNodeTest

--- de.topicmapslab.tmcledit.tests/src/de/topicmapslab/tmcledit/model/compare/TypeNodeComparator.java
+++ de.topicmapslab.tmcledit.tests/src/de/topicmapslab/tmcledit/model/compare/TypeNodeComparator.java
@@ -5,17 +5,8 @@
  */
 package de.topicmapslab.tmcledit.model.compare;
 
-import de.topicmapslab.tmcledit.model.ModelFactory;
 import de.topicmapslab.tmcledit.model.TypeNode;
 
-import junit.textui.TestRunner;
-
-/**
- * <!-- begin-user-doc --> A test case for the model object '
- * <em><b>Type Node</b></em>'. <!-- end-user-doc -->
- * 
- * @generated
- */
 public class TypeNodeComparator extends NodeComparator {
 
 	public boolean equals(TypeNode o1, TypeNode o2) {
@@ -28,7 +19,7 @@ public class TypeNodeComparator extends NodeComparator {
 
 		if (super.equals(o1, o2) == false)
 			return false;
-		
+
 		if (stringCompare(o1.getImage(), o2.getImage()) == false)
 			return false;
 
