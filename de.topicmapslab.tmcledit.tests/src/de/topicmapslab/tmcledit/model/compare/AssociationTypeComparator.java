706e1c09f4cb013ba1fe273de302d79ab578a28a
lippert <lippert@localhost>
2010-04-07 17:54:08 +0200

    Implemented AssociationTypeTest

--- de.topicmapslab.tmcledit.tests/src/de/topicmapslab/tmcledit/model/compare/AssociationTypeComparator.java
+++ de.topicmapslab.tmcledit.tests/src/de/topicmapslab/tmcledit/model/compare/AssociationTypeComparator.java
@@ -6,68 +6,13 @@
 package de.topicmapslab.tmcledit.model.compare;
 
 import de.topicmapslab.tmcledit.model.AssociationType;
-import de.topicmapslab.tmcledit.model.ModelFactory;
 
-import junit.textui.TestRunner;
-
-/**
- * <!-- begin-user-doc -->
- * A test case for the model object '<em><b>Association Type</b></em>'.
- * <!-- end-user-doc -->
- * @generated
- */
 public class AssociationTypeComparator extends ScopedTopicTypeComparator {
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	public static void main(String[] args) {
-		TestRunner.run(AssociationTypeComparator.class);
+	public boolean equals(AssociationType o1, AssociationType o2) {
+
+		return true;
+
 	}
 
-	/**
-	 * Constructs a new Association Type test case with the given name.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	public AssociationTypeComparator(String name) {
-		super(name);
-	}
-
-	/**
-	 * Returns the fixture for this Association Type test case.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	@Override
-	protected AssociationType getFixture() {
-		return (AssociationType)fixture;
-	}
-
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @see junit.framework.TestCase#setUp()
-	 * @generated
-	 */
-	@Override
-	protected void setUp() throws Exception {
-		setFixture(ModelFactory.eINSTANCE.createAssociationType());
-	}
-
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @see junit.framework.TestCase#tearDown()
-	 * @generated
-	 */
-	@Override
-	protected void tearDown() throws Exception {
-		setFixture(null);
-	}
-
-} //AssociationTypeTest
+} // AssociationTypeTest
