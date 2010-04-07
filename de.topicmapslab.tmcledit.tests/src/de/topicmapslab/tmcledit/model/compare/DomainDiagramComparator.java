0aec8fc59573a376aaea6c649ce1c589e84b849c
lippert <lippert@localhost>
2010-04-07 20:06:55 +0200

    Implemented DomainDiagramTest

---
+++ de.topicmapslab.tmcledit.tests/src/de/topicmapslab/tmcledit/model/compare/DomainDiagramComparator.java
@@ -0,0 +1,18 @@
+/**
+ * (C) 2008 Hannes Niederhausen, Topic Maps Lab
+ *
+ * $Id$
+ */
+package de.topicmapslab.tmcledit.model.compare;
+
+import de.topicmapslab.tmcledit.model.DomainDiagram;
+
+public class DomainDiagramComparator extends DiagramComparator {
+
+	public boolean equals(DomainDiagram o1, DomainDiagram o2) {
+
+		return true;
+
+	}
+
+} // DomainDiagramTest
--- de.topicmapslab.tmcledit.tests/src/de/topicmapslab/tmcledit/model/tests/DomainDiagramTest.java
+++ de.topicmapslab.tmcledit.tests/src/de/topicmapslab/tmcledit/model/tests/DomainDiagramTest.java
@@ -5,69 +5,257 @@
  */
 package de.topicmapslab.tmcledit.model.tests;
 
-import de.topicmapslab.tmcledit.model.DomainDiagram;
-import de.topicmapslab.tmcledit.model.ModelFactory;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
+import de.topicmapslab.tmcledit.model.Comment;
+import de.topicmapslab.tmcledit.model.Diagram;
+import de.topicmapslab.tmcledit.model.DomainDiagram;
+import de.topicmapslab.tmcledit.model.Edge;
+import de.topicmapslab.tmcledit.model.EdgeType;
+import de.topicmapslab.tmcledit.model.ModelFactory;
+import de.topicmapslab.tmcledit.model.Node;
+import de.topicmapslab.tmcledit.model.compare.DiagramComparator;
+import de.topicmapslab.tmcledit.model.compare.DomainDiagramComparator;
+
+import junit.framework.Assert;
 import junit.textui.TestRunner;
 
 /**
- * <!-- begin-user-doc -->
- * A test case for the model object '<em><b>Domain Diagram</b></em>'.
- * <!-- end-user-doc -->
+ * <!-- begin-user-doc --> A test case for the model object '
+ * <em><b>Domain Diagram</b></em>'. <!-- end-user-doc -->
+ * 
  * @generated
  */
 public class DomainDiagramTest extends DiagramTest {
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	public static void main(String[] args) {
-		TestRunner.run(DomainDiagramTest.class);
+	private DomainDiagram testObject1;
+	private DomainDiagram testObject2;
+	private DomainDiagramComparator comp;
+
+	@Before
+	public void prepare() {
+
+		if (testObject1 == null)
+			testObject1 = ModelFactory.eINSTANCE.createDomainDiagram();
+		if (testObject2 == null)
+			testObject2 = ModelFactory.eINSTANCE.createDomainDiagram();
+
+		comp = new DomainDiagramComparator();
+	}
+
+	@After
+	public void shutdown() {
+		testObject1 = null;
+		testObject2 = null;
 	}
 
 	/**
-	 * Constructs a new Domain Diagram test case with the given name.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
+	 * Test compares Object with NULL and proves the equality of two NULL
+	 * Objects
 	 */
-	public DomainDiagramTest(String name) {
-		super(name);
+
+	@Test
+	public void nullTest() {
+
+		Assert.assertFalse(comp.equals(testObject1, null));
+		Assert.assertFalse(comp.equals(null, testObject1));
+		Assert.assertTrue(comp.equals(null, null));
 	}
 
 	/**
-	 * Returns the fixture for this Domain Diagram test case.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
+	 * Test compares two objects with different and same IDs.
 	 */
-	@Override
-	protected DomainDiagram getFixture() {
-		return (DomainDiagram)fixture;
+
+	@Test
+	public void idTest() {
+
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		testObject1.setId(testObject2.getId());
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @see junit.framework.TestCase#setUp()
-	 * @generated
+	 * Test compares two objects with different and same names.
 	 */
-	@Override
-	protected void setUp() throws Exception {
-		setFixture(ModelFactory.eINSTANCE.createDomainDiagram());
+
+	@Test
+	public void nameTest() {
+
+		testObject1.setId(testObject2.getId());
+
+		testObject1.setName("TMCL");
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		testObject2.setName("TMQL");
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		testObject2.setName("TMCL");
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @see junit.framework.TestCase#tearDown()
-	 * @generated
+	 * Test compares two objects with different and same comments.
 	 */
-	@Override
-	protected void tearDown() throws Exception {
-		setFixture(null);
+
+	@Test
+	public void commentTest() {
+
+		testObject1.setId(testObject2.getId());
+
+		Comment comment1 = ModelFactory.eINSTANCE.createComment();
+		Comment comment2 = ModelFactory.eINSTANCE.createComment();
+		Comment comment3 = ModelFactory.eINSTANCE.createComment();
+		Comment comment4 = ModelFactory.eINSTANCE.createComment();
+
+		// add the same comment which is not possible
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+		testObject1.getComments().add(comment1);
+		testObject2.getComments().add(comment1);
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+
+		// add an equal comment into list #1
+		testObject1.getComments().add(comment2);
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		comment2.setId(comment1.getId());
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+
+		// test different list lengths and different entries
+		testObject1.getComments().add(comment3);
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		testObject2.getComments().add(comment4);
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		comment4.setId(comment3.getId());
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+
+		// edit some values from entry #2
+		comment3.setContent("TMCL");
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		comment4.setContent("TMQL");
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		comment4.setContent("TMCL");
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+
+		comment3.setHeight(1);
+		comment4.setHeight(2);
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		comment4.setHeight(1);
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+
+		comment3.setPosX(1);
+		comment4.setPosX(2);
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		comment4.setPosX(1);
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
 	}
 
-} //DomainDiagramTest
+	/**
+	 * Test compares two objects with different and same edges.
+	 */
+
+	@Test
+	public void edgeTest() {
+
+		testObject1.setId(testObject2.getId());
+
+		Edge edge1 = ModelFactory.eINSTANCE.createEdge();
+		Edge edge2 = ModelFactory.eINSTANCE.createEdge();
+		Edge edge3 = ModelFactory.eINSTANCE.createEdge();
+		Edge edge4 = ModelFactory.eINSTANCE.createEdge();
+
+		// add the same edge which is not possible
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+		testObject1.getEdges().add(edge1);
+		testObject2.getEdges().add(edge1);
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+
+		// add an equal edge into list #1
+		testObject1.getEdges().add(edge2);
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		edge2.setId(edge1.getId());
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+
+		// test different list lengths and different entries
+		testObject1.getEdges().add(edge3);
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		testObject2.getEdges().add(edge4);
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		edge4.setId(edge3.getId());
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+
+		// edit some values from entry #2
+		Node node1 = ModelFactory.eINSTANCE.createNode();
+		Node node2 = ModelFactory.eINSTANCE.createNode();
+
+		edge3.setSource(node1);
+		edge4.setSource(node2);
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		node2.setId(node1.getId());
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+		node1.setPosX(1);
+		node2.setPosX(2);
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		node2.setPosX(1);
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+
+		EdgeType edType1 = EdgeType.ROLE_CONSTRAINT_TYPE;
+		EdgeType edType2 = EdgeType.AKO_TYPE;
+
+		edge3.setType(edType1);
+		edge4.setType(edType2);
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		edge4.setType(edType1);
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+
+	}
+
+	/**
+	 * Test compares two objects with different and same nodes.
+	 */
+
+	@Test
+	public void nodeTest() {
+
+		testObject1.setId(testObject2.getId());
+
+		Node node1 = ModelFactory.eINSTANCE.createNode();
+		Node node2 = ModelFactory.eINSTANCE.createNode();
+		Node node3 = ModelFactory.eINSTANCE.createNode();
+		Node node4 = ModelFactory.eINSTANCE.createNode();
+
+		// add the same nodes which is not possible
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+		testObject1.getNodes().add(node1);
+		testObject2.getNodes().add(node1);
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+
+		// add an equal node into list #1
+		testObject1.getNodes().add(node2);
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		node2.setId(node1.getId());
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+
+		// test different list lengths and different entries
+		testObject1.getNodes().add(node3);
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		testObject2.getNodes().add(node4);
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		node4.setId(node3.getId());
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+
+		// edit some values from entry #2
+		node3.setPosX(1);
+		node4.setPosX(2);
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		node4.setPosX(1);
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+
+		node3.setPosY(1);
+		node4.setPosY(2);
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		node4.setPosY(1);
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+	
+	}
+
+} // DomainDiagramTest
