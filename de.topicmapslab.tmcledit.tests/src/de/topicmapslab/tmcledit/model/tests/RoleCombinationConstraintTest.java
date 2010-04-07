a49ebb5081aab4cbb2a557926a20dea57e2e4efb
lippert <lippert@localhost>
2010-04-07 18:17:35 +0200

    Implemented RoleCombinationConstraintTest

--- de.topicmapslab.tmcledit.tests/src/de/topicmapslab/tmcledit/model/tests/RoleCombinationConstraintTest.java
+++ de.topicmapslab.tmcledit.tests/src/de/topicmapslab/tmcledit/model/tests/RoleCombinationConstraintTest.java
@@ -5,69 +5,344 @@
  */
 package de.topicmapslab.tmcledit.model.tests;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import de.topicmapslab.tmcledit.model.Annotation;
 import de.topicmapslab.tmcledit.model.ModelFactory;
 import de.topicmapslab.tmcledit.model.RoleCombinationConstraint;
+import de.topicmapslab.tmcledit.model.TopicId;
+import de.topicmapslab.tmcledit.model.TopicType;
+import de.topicmapslab.tmcledit.model.compare.RoleCombinationConstraintComparator;
 
-import junit.textui.TestRunner;
+import junit.framework.Assert;
 
-/**
- * <!-- begin-user-doc -->
- * A test case for the model object '<em><b>Role Combination Constraint</b></em>'.
- * <!-- end-user-doc -->
- * @generated
- */
 public class RoleCombinationConstraintTest extends TMCLConstructTest {
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	public static void main(String[] args) {
-		TestRunner.run(RoleCombinationConstraintTest.class);
+	private RoleCombinationConstraint testObject1;
+	private RoleCombinationConstraint testObject2;
+	private RoleCombinationConstraintComparator comp;
+
+	@Before
+	public void prepare() {
+
+		if (testObject1 == null)
+			testObject1 = ModelFactory.eINSTANCE
+					.createRoleCombinationConstraint();
+		if (testObject2 == null)
+			testObject2 = ModelFactory.eINSTANCE
+					.createRoleCombinationConstraint();
+
+		comp = new RoleCombinationConstraintComparator();
+	}
+
+	@After
+	public void shutdown() {
+		testObject1 = null;
+		testObject2 = null;
 	}
 
 	/**
-	 * Constructs a new Role Combination Constraint test case with the given name.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
+	 * Test compares Object with NULL and proves the equality of two NULL
+	 * Objects
 	 */
-	public RoleCombinationConstraintTest(String name) {
-		super(name);
+
+	@Test
+	public void nullTest() {
+
+		Assert.assertFalse(comp.equals(testObject1, null));
+		Assert.assertFalse(comp.equals(null, testObject1));
+		Assert.assertTrue(comp.equals(null, null));
 	}
 
 	/**
-	 * Returns the fixture for this Role Combination Constraint test case.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
+	 * Test compares two objects with different and same IDs.
 	 */
-	@Override
-	protected RoleCombinationConstraint getFixture() {
-		return (RoleCombinationConstraint)fixture;
+
+	@Test
+	public void idTest() {
+
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		testObject1.setId(testObject2.getId());
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @see junit.framework.TestCase#setUp()
-	 * @generated
+	 * Test compares two objects with different and equal see_Also values.
 	 */
-	@Override
-	protected void setUp() throws Exception {
-		setFixture(ModelFactory.eINSTANCE.createRoleCombinationConstraint());
+
+	@Test
+	public void seeAlsoTest() {
+
+		testObject1.setId(testObject2.getId());
+
+		testObject1.setSee_also("TMCL");
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		testObject2.setSee_also("TMCL");
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @see junit.framework.TestCase#tearDown()
-	 * @generated
+	 * Test compares two objects with different and same descriptions.
 	 */
-	@Override
-	protected void tearDown() throws Exception {
-		setFixture(null);
+
+	@Test
+	public void descriptionTest() {
+
+		testObject1.setId(testObject2.getId());
+
+		testObject1.setDescription("TMCL");
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		testObject2.setDescription("TMCL");
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
 	}
 
-} //RoleCombinationConstraintTest
+	/**
+	 * Test compares two objects with different and same comments.
+	 */
+
+	@Test
+	public void commentTest() {
+
+		testObject1.setId(testObject2.getId());
+
+		testObject1.setComment("TMCL");
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		testObject2.setComment("TMCL");
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+	}
+
+	/**
+	 * Test compares objects with different and equal annotations.
+	 */
+
+	@Test
+	public void annotationTest() {
+
+		testObject1.setId(testObject2.getId());
+		Annotation annotation1 = ModelFactory.eINSTANCE.createAnnotation();
+		Annotation annotation2 = ModelFactory.eINSTANCE.createAnnotation();
+		Annotation annotation3 = ModelFactory.eINSTANCE.createAnnotation();
+		Annotation annotation4 = ModelFactory.eINSTANCE.createAnnotation();
+
+		// add annotation to testObject1
+		annotation1.setKey("TMCL");
+		annotation1.setValue("TMCL");
+		testObject1.getAnnotations().add(annotation1);
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+
+		// add a different annotation to testObject2
+		annotation2.setId(annotation1.getId());
+		annotation2.setKey("TMC");
+		annotation2.setValue("TMC");
+		testObject2.getAnnotations().add(annotation2);
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+
+		// make both annotations the same
+		annotation2.setKey("TMCL");
+		annotation2.setValue("TMCL");
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+
+		// add the same annotation into both lists
+		testObject1.getAnnotations().add(annotation3);
+		testObject2.getAnnotations().add(annotation3);
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+
+		// make list length equal, but at the first run not entry #1
+		testObject1.getAnnotations().add(annotation4);
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		annotation4.setId(annotation3.getId());
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+
+	}
+
+	/**
+	 * Test compares objects with different and equal otherPlayers
+	 */
+
+	@Test
+	public void otherPlayerTest() {
+
+		testObject1.setId(testObject2.getId());
+
+		TopicType topicType1 = ModelFactory.eINSTANCE.createTopicType();
+		TopicType topicType2 = ModelFactory.eINSTANCE.createTopicType();
+
+		// set different topic types
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+		testObject1.setOtherPlayer(topicType1);
+		testObject2.setOtherPlayer(topicType2);
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+
+		// make both topic types the same
+		topicType1.setId(topicType2.getId());
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+
+		// change some values of the topic types
+		topicType1.setAbstract(true);
+		topicType2.setAbstract(false);
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		topicType2.setAbstract(true);
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+
+		topicType1.setName("TMCL");
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		topicType2.setName("TMQL");
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		topicType2.setName("TMCL");
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+
+		TopicId topicId1 = TopicId.SUBJECT_IDENTIFIER;
+		TopicId topicId2 = TopicId.ITEM_IDENTIFIER;
+
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+		topicType1.setIdType(topicId1);
+		topicType2.setIdType(topicId2);
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		topicType1.setIdType(topicId2);
+
+	}
+
+	/**
+	 * Test compares objects with different and equal otherRoles
+	 */
+
+	@Test
+	public void otherRoleTest() {
+
+		testObject1.setId(testObject2.getId());
+
+		TopicType topicType1 = ModelFactory.eINSTANCE.createTopicType();
+		TopicType topicType2 = ModelFactory.eINSTANCE.createTopicType();
+
+		// set different topic types
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+		testObject1.setOtherRole(topicType1);
+		testObject2.setOtherRole(topicType2);
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+
+		// make both topic types the same
+		topicType1.setId(topicType2.getId());
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+
+		// change some values of the topic types
+		topicType1.setAbstract(true);
+		topicType2.setAbstract(false);
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		topicType2.setAbstract(true);
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+
+		topicType1.setName("TMCL");
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		topicType2.setName("TMQL");
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		topicType2.setName("TMCL");
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+
+		TopicId topicId1 = TopicId.SUBJECT_IDENTIFIER;
+		TopicId topicId2 = TopicId.ITEM_IDENTIFIER;
+
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+		topicType1.setIdType(topicId1);
+		topicType2.setIdType(topicId2);
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		topicType1.setIdType(topicId2);
+
+	}
+
+	/**
+	 * Test compares objects with different and equal players
+	 */
+
+	@Test
+	public void playerTest() {
+
+		testObject1.setId(testObject2.getId());
+
+		TopicType topicType1 = ModelFactory.eINSTANCE.createTopicType();
+		TopicType topicType2 = ModelFactory.eINSTANCE.createTopicType();
+
+		// set different topic types
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+		testObject1.setPlayer(topicType1);
+		testObject2.setPlayer(topicType2);
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+
+		// make both topic types the same
+		topicType1.setId(topicType2.getId());
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+
+		// change some values of the topic types
+		topicType1.setAbstract(true);
+		topicType2.setAbstract(false);
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		topicType2.setAbstract(true);
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+
+		topicType1.setName("TMCL");
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		topicType2.setName("TMQL");
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		topicType2.setName("TMCL");
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+
+		TopicId topicId1 = TopicId.SUBJECT_IDENTIFIER;
+		TopicId topicId2 = TopicId.ITEM_IDENTIFIER;
+
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+		topicType1.setIdType(topicId1);
+		topicType2.setIdType(topicId2);
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		topicType1.setIdType(topicId2);
+
+	}
+
+	/**
+	 * Test compares objects with different and equal roles.
+	 */
+
+	@Test
+	public void roleTest() {
+
+		testObject1.setId(testObject2.getId());
+
+		TopicType topicType1 = ModelFactory.eINSTANCE.createTopicType();
+		TopicType topicType2 = ModelFactory.eINSTANCE.createTopicType();
+
+		// set different topic types
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+		testObject1.setRole(topicType1);
+		testObject2.setRole(topicType2);
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+
+		// make both topic types the same
+		topicType1.setId(topicType2.getId());
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+
+		// change some values of the topic types
+		topicType1.setAbstract(true);
+		topicType2.setAbstract(false);
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		topicType2.setAbstract(true);
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+
+		topicType1.setName("TMCL");
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		topicType2.setName("TMQL");
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		topicType2.setName("TMCL");
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+
+		TopicId topicId1 = TopicId.SUBJECT_IDENTIFIER;
+		TopicId topicId2 = TopicId.ITEM_IDENTIFIER;
+
+		Assert.assertTrue(comp.equals(testObject1, testObject2));
+		topicType1.setIdType(topicId1);
+		topicType2.setIdType(topicId2);
+		Assert.assertFalse(comp.equals(testObject1, testObject2));
+		topicType1.setIdType(topicId2);
+
+	}
+
+} // RoleCombinationConstraintTest
