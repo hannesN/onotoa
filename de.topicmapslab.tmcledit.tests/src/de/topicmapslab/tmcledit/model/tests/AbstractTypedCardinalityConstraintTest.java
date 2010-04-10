35a215eca8d8e92a4870f39fb8d119005b4610ca
lippert <lippert@localhost>
2010-04-10 16:00:47 +0200

    Done!

--- de.topicmapslab.tmcledit.tests/src/de/topicmapslab/tmcledit/model/tests/AbstractTypedCardinalityConstraintTest.java
+++ de.topicmapslab.tmcledit.tests/src/de/topicmapslab/tmcledit/model/tests/AbstractTypedCardinalityConstraintTest.java
@@ -5,69 +5,7 @@
  */
 package de.topicmapslab.tmcledit.model.tests;
 
-import de.topicmapslab.tmcledit.model.AbstractTypedCardinalityConstraint;
-import de.topicmapslab.tmcledit.model.ModelFactory;
+public class AbstractTypedCardinalityConstraintTest extends
+		AbstractCardinalityConstraintTest {
 
-import junit.textui.TestRunner;
-
-/**
- * <!-- begin-user-doc -->
- * A test case for the model object '<em><b>Abstract Typed Cardinality Constraint</b></em>'.
- * <!-- end-user-doc -->
- * @generated
- */
-public class AbstractTypedCardinalityConstraintTest extends AbstractCardinalityConstraintTest {
-
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	public static void main(String[] args) {
-		TestRunner.run(AbstractTypedCardinalityConstraintTest.class);
-	}
-
-	/**
-	 * Constructs a new Abstract Typed Cardinality Constraint test case with the given name.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	public AbstractTypedCardinalityConstraintTest(String name) {
-		super(name);
-	}
-
-	/**
-	 * Returns the fixture for this Abstract Typed Cardinality Constraint test case.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	@Override
-	protected AbstractTypedCardinalityConstraint getFixture() {
-		return (AbstractTypedCardinalityConstraint)fixture;
-	}
-
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @see junit.framework.TestCase#setUp()
-	 * @generated
-	 */
-	@Override
-	protected void setUp() throws Exception {
-		setFixture(ModelFactory.eINSTANCE.createAbstractTypedCardinalityConstraint());
-	}
-
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @see junit.framework.TestCase#tearDown()
-	 * @generated
-	 */
-	@Override
-	protected void tearDown() throws Exception {
-		setFixture(null);
-	}
-
-} //AbstractTypedCardinalityConstraintTest
+} // AbstractTypedCardinalityConstraintTest
--- de.topicmapslab.tmcledit.tests/src/de/topicmapslab/tmcledit/model/tests/AbstractTypedConstraintTest.java
+++ de.topicmapslab.tmcledit.tests/src/de/topicmapslab/tmcledit/model/tests/AbstractTypedConstraintTest.java
@@ -5,14 +5,6 @@
  */
 package de.topicmapslab.tmcledit.model.tests;
 
-import de.topicmapslab.tmcledit.model.AbstractTypedConstraint;
-
-/**
- * <!-- begin-user-doc -->
- * A test case for the model object '<em><b>Abstract Typed Constraint</b></em>'.
- * <!-- end-user-doc -->
- * @generated
- */
 public abstract class AbstractTypedConstraintTest extends TMCLConstructTest {
 
-} //AbstractTypedConstraintTest
+} // AbstractTypedConstraintTest
