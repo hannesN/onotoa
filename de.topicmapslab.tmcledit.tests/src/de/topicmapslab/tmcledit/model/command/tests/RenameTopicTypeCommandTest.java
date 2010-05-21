6253f0d5859ac23341f6126a104305b0ea2ecb4d
lippert <lippert@localhost>
2010-05-21 19:12:39 +0200

    Started to implement RenameTopicTypeCommandTest

---
+++ de.topicmapslab.tmcledit.tests/src/de/topicmapslab/tmcledit/model/command/tests/RenameTopicTypeCommandTest.java
@@ -0,0 +1,165 @@
+/*******************************************************************************
+ * Copyright (c) 2008, 2009 Topic Maps Lab and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Hannes Niederhausen - initial API and implementation
+ *******************************************************************************/
+/**
+ * 
+ */
+package de.topicmapslab.tmcledit.model.command.tests;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import de.topicmapslab.tmcledit.model.File;
+import de.topicmapslab.tmcledit.model.ModelFactory;
+import de.topicmapslab.tmcledit.model.TopicMapSchema;
+import de.topicmapslab.tmcledit.model.TopicType;
+import de.topicmapslab.tmcledit.model.commands.RenameTopicTypeCommand;
+import de.topicmapslab.tmcledit.model.index.ModelIndexer;
+
+/**
+ * @author Hannes Niederhausen
+ * 
+ */
+public class RenameTopicTypeCommandTest {
+
+	private final String BASELOCATOR = "http://onotoa.topicmapslab.de/";
+
+	private RenameTopicTypeCommand command;
+	private TopicType topicType0;
+	private TopicType topicType1;
+	private String oldName;
+	private String newName;
+	private String otherName;
+	private TopicMapSchema schema;
+	private File file;
+
+	@Before
+	public void prepare() {
+
+		if (oldName == null)
+			oldName = "oldName";
+
+		if (newName == null)
+			newName = "newName";
+
+		if (otherName == null)
+			otherName = "otherName";
+
+		if (topicType0 == null) {
+
+			topicType0 = ModelFactory.eINSTANCE.createTopicType();
+			topicType0.setName(oldName);
+
+		}
+
+		if (topicType1 == null) {
+
+			topicType1 = ModelFactory.eINSTANCE.createTopicType();
+			topicType1.setName(otherName);
+
+		}
+
+		if (schema == null) {
+
+			schema = ModelFactory.eINSTANCE.createTopicMapSchema();
+			schema.getTopicTypes().add(topicType0);
+			schema.getTopicTypes().add(topicType1);
+			schema.setBaseLocator(BASELOCATOR);
+
+		}
+
+		if (file == null) {
+
+			file = ModelFactory.eINSTANCE.createFile();
+			file.setTopicMapSchema(schema);
+
+		}
+
+		ModelIndexer.createInstance(file);
+
+		if (command == null)
+			command = new RenameTopicTypeCommand(topicType0, newName);
+
+	}
+
+	@After
+	public void shutdown() {
+
+		oldName = null;
+		newName = null;
+		otherName = null;
+		topicType0 = null;
+		topicType1 = null;
+		schema = null;
+		file = null;
+		command = null;
+
+	}
+
+	@Test
+	public void canExecuteTest() {
+
+		Assert.assertTrue(command.canExecute());
+
+		command = new RenameTopicTypeCommand(topicType0, otherName);
+		Assert.assertFalse(command.canExecute());
+
+		command = new RenameTopicTypeCommand(topicType0, "");
+		Assert.assertFalse(command.canExecute());
+
+	}
+
+	@Test
+	public void executeTest() {
+
+		Assert.assertTrue(command.canExecute());
+		command.execute();
+
+		Assert.assertEquals(newName, topicType0.getName());
+
+	}
+
+	@Test
+	public void canUndo() {
+
+		Assert.assertTrue(command.canExecute());
+		command.execute();
+		Assert.assertTrue(command.canUndo());
+
+	}
+
+	@Test
+	public void undoTest() {
+
+		Assert.assertTrue(command.canExecute());
+		command.execute();
+		Assert.assertTrue(command.canUndo());
+		command.undo();
+
+		Assert.assertEquals(oldName, topicType0.getName());
+
+	}
+
+	@Test
+	public void redoTest() {
+
+		Assert.assertTrue(command.canExecute());
+		command.execute();
+		Assert.assertTrue(command.canUndo());
+		command.undo();
+		command.redo();
+
+		Assert.assertEquals(newName, topicType0.getName());
+
+	}
+
+}
