30206358c38d736b1b0461edd2290e749c28c87f
lippert <lippert@localhost>
2010-07-28 15:50:50 +0200

    Added plugin to handle interaction between doubleclick in the search view and property details view

--- de.topicmapslab.onotoa.search/src/de/topicmapslab/onotoa/search/Activator.java
+++ de.topicmapslab.onotoa.search/src/de/topicmapslab/onotoa/search/Activator.java
@@ -2,6 +2,9 @@ package de.topicmapslab.onotoa.search;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+
+import de.topicmapslab.onotoa.selection.service.IOnotoaSelectionService;
 
 /**
  * The activator class controls the plug-in life cycle
@@ -13,6 +16,8 @@ public class Activator extends AbstractUIPlugin {
 
 	// The shared instance
 	private static Activator plugin;
+	
+	private IOnotoaSelectionService selectionService;
 	
 	/**
 	 * The constructor
@@ -27,6 +32,8 @@ public class Activator extends AbstractUIPlugin {
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
+		ServiceReference servRef = context.getServiceReference(IOnotoaSelectionService.class.getName());
+		selectionService = (IOnotoaSelectionService) context.getService(servRef);
 	}
 
 	/*
@@ -47,4 +54,11 @@ public class Activator extends AbstractUIPlugin {
 		return plugin;
 	}
 
+	/**
+     * @return the selectionService
+     */
+    public IOnotoaSelectionService getSelectionService() {
+	    return selectionService;
+    }
+    
 }
