c6fb30bf7214f3f2f6b120e554ad97748c0e6a45
lippert <lippert@localhost>
2010-07-28 15:47:04 +0200

    Imlmented class for switching between basic and advanced search of TopicTypes

---
+++ de.topicmapslab.onotoa.search/src/de/topicmapslab/onotoa/search/searchImpl/MainSearchHandler.java
@@ -0,0 +1,65 @@
+/*******************************************************************************
+ * Copyright (c) 2008, 2009 Topic Maps Lab and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Hannes Niederhausen - initial API and implementation
+ *******************************************************************************/
+package de.topicmapslab.onotoa.search.searchImpl;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+
+import de.topicmapslab.onotoa.search.util.SearchDataObject;
+import de.topicmapslab.onotoa.search.views.Container;
+import de.topicmapslab.tmcledit.model.TopicMapSchema;
+
+/**
+ * Class detects the modus of the search (basic, advanced) and the searched type
+ * (TopicType, NameType ...) After this it initiates the correct search
+ * 
+ * @author Sebastian Lippert
+ * 
+ */
+
+public class MainSearchHandler {
+
+	private Container con;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param searchObj
+	 *            Object that includes all inputs from the search mask
+	 * @param schema
+	 *            the search based on
+	 */
+
+	public MainSearchHandler(SearchDataObject searchObj, TopicMapSchema schema, IProgressMonitor progressMonitor) {
+
+		con = new Container();
+
+		// detect the modus of the search by the according Boolean
+		if (!searchObj.getIsAdvancedSearch()) {
+
+			BasicTopicTypeSearcher searcher = new BasicTopicTypeSearcher(
+					searchObj, schema, progressMonitor);
+			searcher.fetchResult();
+			con = searcher.getResult();
+
+		}
+	}
+
+	/**
+	 * Getter for the search result
+	 * 
+	 * @return Result container
+	 */
+
+	public Container getResultContainer() {
+		return con;
+	}
+
+}
