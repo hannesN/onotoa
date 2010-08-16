0c9dfe45c660331dc5e57ad5118ea6ac09ddfefe
lippert <lippert@localhost>
2010-08-17 01:22:10 +0200

    Implemented new GUI according to new search policy

--- de.topicmapslab.onotoa.search/src/de/topicmapslab/onotoa/search/dialogs/BasicTopicTypeSearchTab.java
+++ de.topicmapslab.onotoa.search/src/de/topicmapslab/onotoa/search/dialogs/BasicTopicTypeSearchTab.java
@@ -18,8 +18,12 @@ import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
+
+import de.topicmapslab.tmcledit.model.KindOfTopicType;
 
 /**
  * @author sip
@@ -27,50 +31,142 @@ import org.eclipse.swt.widgets.Text;
  */
 public class BasicTopicTypeSearchTab {
 
+	private AdvancedTopicTypeSearch advancedTopic, advancedAssosiation;
+
+	private Shell dockingShell;
 	private Combo typeBox;
+	private Text nameField;
 	private Button caseButton, matchButton, regularButton;
-	private String itemsBasic[] = { "Any", "TopicType", "OccurrenceType", "NameType", "RoleType", "AssociationType" };
-	private Composite comp;
+	private Button checkSI, checkSL, advancedButton;
+	private Composite mainComp, comp;
+	private Composite compTopic, compAssociation;
+
+	private String itemsBasic[] = { "Any", "Topic Type", "Occurrence Type", "Name Type", "Role Type",
+	        "Association Type" };
+	private boolean isAdvanced = false;
 
 	public BasicTopicTypeSearchTab(Composite parent) {
 
 		Label label;
+		Group group;
 		GridData gridData;
+		Composite buttonBar;
+
+		dockingShell = new Shell();
+
+		mainComp = new Composite(parent, SWT.None);
+		mainComp.setLayout(new GridLayout());
+		mainComp.setLayoutData(new GridData(GridData.FILL_BOTH));
 
 		comp = new Composite(parent, SWT.NONE);
 		comp.setLayoutData(new GridData(GridData.FILL_BOTH));
 		comp.setLayout(new GridLayout(2, false));
 
-		label = new Label(comp, SWT.NONE);
-		label.setText("Search");
+		gridData = new GridData(GridData.FILL_HORIZONTAL);
+		gridData.horizontalSpan = 2;
 
-		nameField = new Text(comp, SWT.SINGLE | SWT.BORDER);
-		gridData = new GridData(GridData.HORIZONTAL_ALIGN_FILL);
-		nameField.setLayoutData(gridData);
-
-		label = new Label(comp, SWT.NONE);
-		label.setText("Type");
-
-		typeBox = new Combo(comp, SWT.NONE | SWT.READ_ONLY);
-		typeBox.setItems(itemsBasic);
-		typeBox.select(0);
-		typeBox.setLayoutData(gridData);
+		group = new Group(comp, SWT.SHADOW_OUT);
+		group.setLayout(new GridLayout(2, false));
+		group.setLayoutData(gridData);
+		group.setText("Search For");
 
 		gridData = new GridData(GridData.FILL_HORIZONTAL);
 		gridData.horizontalSpan = 2;
 
-		caseButton = new Button(comp, SWT.CHECK);
+		nameField = new Text(group, SWT.SINGLE | SWT.BORDER);
+		nameField.setLayoutData(gridData);
+
+		label = new Label(group, SWT.NONE);
+		label.setText("Type");
+
+		typeBox = new Combo(group, SWT.NONE | SWT.READ_ONLY);
+		typeBox.setItems(itemsBasic);
+		typeBox.select(0);
+		typeBox.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+
+		hookTypeListener();
+
+		gridData = new GridData(GridData.FILL_HORIZONTAL);
+		gridData.horizontalSpan = 2;
+
+		buttonBar = new Composite(group, SWT.NONE);
+		buttonBar.setLayout(new GridLayout(2, false));
+		buttonBar.setLayoutData(gridData);
+
+		caseButton = new Button(buttonBar, SWT.CHECK);
 		caseButton.setText("Case Sensitive");
-		caseButton.setLayoutData(gridData);
 
-		matchButton = new Button(comp, SWT.CHECK);
+		checkSI = new Button(buttonBar, SWT.CHECK);
+		checkSI.setText("Check Subject Identifier");
+
+		matchButton = new Button(buttonBar, SWT.CHECK);
 		matchButton.setText("Exact Match");
-		matchButton.setLayoutData(gridData);
 
-		regularButton = new Button(comp, SWT.CHECK);
+		checkSL = new Button(buttonBar, SWT.CHECK);
+		checkSL.setText("Check Subject Locator");
+
+		regularButton = new Button(buttonBar, SWT.CHECK);
 		regularButton.setText("Regular Expression");
-		regularButton.setLayoutData(gridData);
-		hookButtonSelectionListener();
+
+		hookRegExpListener();
+
+		advancedButton = new Button(comp, SWT.PUSH);
+		advancedButton.setText("Advanced >>");
+		advancedButton.setToolTipText("Enable advanced option to specify the seach");
+		advancedButton.setEnabled(false);
+
+		hookAdvancedListener();
+
+		/*
+		 * instantiate advanced parts and dock them to invisible shell to use
+		 * them later in a dynamic way
+		 */
+		advancedTopic = new AdvancedTopicTypeSearch();
+		advancedAssosiation = new AdvancedTopicTypeSearch();
+
+		compTopic = advancedTopic.getAdvancedTopicComposite();
+		compAssociation = advancedAssosiation.getAdvancedAssociationComposite();
+
+	}
+
+	private void hookAdvancedListener() {
+
+		advancedButton.addSelectionListener(new SelectionAdapter() {
+
+			public void widgetSelected(SelectionEvent e) {
+
+				if (!isAdvanced) {
+					isAdvanced = true;
+					advancedButton.setText("Advanced <<");
+
+					if (typeBox.getSelectionIndex() == KindOfTopicType.TOPIC_TYPE_VALUE + 1) {
+
+						compAssociation.setParent(dockingShell);
+						compTopic.setParent(comp);
+						comp.layout(true);
+
+					}
+					if (typeBox.getSelectionIndex() == KindOfTopicType.ASSOCIATION_TYPE_VALUE + 1) {
+
+						compTopic.setParent(dockingShell);
+						compAssociation.setParent(comp);
+						comp.layout(true);
+
+					}
+
+				} else {
+
+					compAssociation.setParent(dockingShell);
+					compTopic.setParent(dockingShell);
+
+					isAdvanced = false;
+					advancedButton.setText("Advanced >>");
+
+				}
+
+			}
+
+		});
 
 	}
 
@@ -78,12 +174,13 @@ public class BasicTopicTypeSearchTab {
 	 * Listener for pressed dialog buttons
 	 */
 
-	private void hookButtonSelectionListener() {
+	private void hookRegExpListener() {
 
 		regularButton.addSelectionListener(new SelectionAdapter() {
 
 			public void widgetSelected(SelectionEvent e) {
-				if (e.widget == regularButton && regularButton.getSelection() == true) {
+
+				if (regularButton.getSelection()) {
 
 					caseButton.setSelection(false);
 					matchButton.setSelection(false);
@@ -93,22 +190,64 @@ public class BasicTopicTypeSearchTab {
 
 				}
 
-				if (e.widget == regularButton && regularButton.getSelection() == false) {
+				if (regularButton.getSelection()) {
 
 					caseButton.setEnabled(true);
 					matchButton.setEnabled(true);
 
 				}
+
 			}
 
 		});
 	}
 
+	private void hookTypeListener() {
+
+		typeBox.addSelectionListener(new SelectionAdapter() {
+
+			public void widgetSelected(SelectionEvent e) {
+
+				switch (typeBox.getSelectionIndex()) {
+
+				case 1:
+					advancedButton.setEnabled(true);
+
+					if (isAdvanced) {
+
+						compAssociation.setParent(dockingShell);
+						compTopic.setParent(comp);
+						comp.layout(true);
+					}
+					break;
+				case 5:
+					advancedButton.setEnabled(true);
+					if (isAdvanced) {
+						compTopic.setParent(dockingShell);
+						compAssociation.setParent(comp);
+						comp.layout(true);
+					}
+					break;
+				default:
+
+					compAssociation.setParent(dockingShell);
+					compTopic.setParent(dockingShell);
+
+					advancedButton.setEnabled(false);
+					advancedButton.setText("Advanced >>");
+
+					break;
+				}
+
+			}
+
+		});
+
+	}
+
 	public Composite getComposite() {
 		return this.comp;
 	}
-
-	private Text nameField;
 
 	/**
 	 * @return the nameField
@@ -143,5 +282,19 @@ public class BasicTopicTypeSearchTab {
 	 */
 	public boolean getRegularButtonValue() {
 		return regularButton.getSelection();
+	}
+
+	/**
+	 * @return the checkSI
+	 */
+	public boolean getCheckSIValue() {
+		return checkSI.getSelection();
+	}
+
+	/**
+	 * @return the checkSL
+	 */
+	public boolean getCheckSLValue() {
+		return checkSL.getSelection();
 	}
 }
