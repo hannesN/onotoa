455ab0032bbc6a1407d3c6b7ee1dec5b4bdb51dd
lippert <lippert@localhost>
2010-09-08 16:08:51 +0200

    New implementation for Non-Identifier search

---
+++ de.topicmapslab.onotoa.search/src/de/topicmapslab/onotoa/search/commands/TopicsWithoutIdentifierSearchHandler.java
@@ -0,0 +1,71 @@
+/*******************************************************************************
+ * Copyright (c) 2008, 2009 Topic Maps Lab and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Hannes Niederhausen - initial API and implementation
+ *******************************************************************************/
+package de.topicmapslab.onotoa.search.commands;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+
+import de.topicmapslab.onotoa.search.searchImpl.TopicsWithoutIdentifierSearcher;
+import de.topicmapslab.onotoa.search.views.SearchView;
+import de.topicmapslab.tmcledit.model.TopicMapSchema;
+import de.topicmapslab.tmcledit.model.views.ModelView;
+
+/**
+ * @author sip
+ * 
+ */
+public class TopicsWithoutIdentifierSearchHandler extends AbstractHandler {
+
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+
+		IWorkbenchWindow activeWorkbenchWindow = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+		IWorkbenchPage activePage = activeWorkbenchWindow.getActivePage();
+		ModelView view = (ModelView) activePage.findView(ModelView.ID);
+
+		if (view == null)
+			return null;
+
+		if (view.getCurrentTopicMapSchema() == null)
+			return null;
+
+		TopicMapSchema schema = view.getCurrentTopicMapSchema();
+		TopicsWithoutIdentifierSearcher searcher = new TopicsWithoutIdentifierSearcher(schema);
+
+		searcher.fetchResult();
+
+		Collections.sort((List<? extends Comparable>) searcher.getResult().getList());
+
+		try {
+
+			SearchView searchView = (SearchView) activePage.findView(SearchView.ID);
+			if (searchView == null)
+				searchView = (SearchView) activePage.showView(SearchView.ID);
+			else
+				activePage.activate(searchView);
+
+			searchView.setContent(searcher.getResult());
+
+		} catch (PartInitException e) {
+			throw new RuntimeException(e);
+		}
+
+		return null;
+	}
+
+}
