f2509b86e6411c5f215de9d47fcbf760eb8da42a
lippert <lippert@localhost>
2010-09-13 18:52:08 +0200

    Added new icons for cleaner

--- de.topicmapslab.tmcledit.edit/src/de/topicmapslab/tmcledit/model/util/ImageConstants.java
+++ de.topicmapslab.tmcledit.edit/src/de/topicmapslab/tmcledit/model/util/ImageConstants.java
@@ -16,39 +16,46 @@ package de.topicmapslab.tmcledit.model.util;
  *
  */
 public interface ImageConstants {
+	
+	public static String ASSOCIATIONCONSTRAINT = "./associationconstraint.gif";
+	public static String ASSOCIATIONCONSTRAINT_SM = "./associationconstraint_sm.gif";
+	public static String ASSOCIATIONROLECONSTRAINT = "./associationroleconstraint.gif";
+	public static String ASSOCIATIONROLECONSTRAINT_SM = "./associationroleconstraint_sm.gif";
+	public static String ASSOCIATIONTYPE = "./associationtype.gif";
+	public static String ASSOCIATIONTYPE_BW = "./associationtype_bw.gif";
+	public static String ASSOCIATIONTYPE_SM = "./associationtype_sm.gif";
+	public static String COMMENT = "./comment.png";
+	public static String DIAGRAM = "./diagram.gif";
+	public static String DOMAINDIAGRAM = "./domaindiagram.gif";
+	public static String ICON = "./icon.png";
+	public static String ISA = "./isa.gif";
 	public static String ISA_SM = "./isa_sm.gif";
+	public static String ITEMIDENTIFIERCONSTRAINT = "./iiconstraint.gif";
+	public static String KINDOF = "./kindof.gif";
+	public static String KINDOF_SM = "./kindof_sm.gif";
+	public static String NAMECONSTRAINT = "./nameconstraint.gif";
+	public static String NAMECONSTRAINT_SM = "./nameconstraint_sm.gif";
+	public static String NAMETYPE = "./nametype.gif";
+	public static String NAMETYPE_BW = "./nametype_bw.gif";
 	public static String NAMETYPE_SM = "./nametype_sm.gif";
 	public static String OCCURRENCECONSTRAINT = "./occurrenceconstraint.gif";
 	public static String OCCURRENCECONSTRAINT_SM = "./occurrenceconstraint_sm.gif";
 	public static String OCCURRENCETYPE = "./occurrencetype.gif";
-	public static String ROLETYPE_SM = "./roletype_sm.gif";
-	public static String NAMECONSTRAINT = "./nameconstraint.gif";
-	public static String ASSOCIATIONROLECONSTRAINT_SM = "./associationroleconstraint_sm.gif";
-	public static String NAMETYPE = "./nametype.gif";
-	public static String ISA = "./isa.gif";
-	public static String ASSOCIATIONCONSTRAINT = "./associationconstraint.gif";
-	public static String KINDOF_SM = "./kindof_sm.gif";
-	public static String ASSOCIATIONROLECONSTRAINT = "./associationroleconstraint.gif";
-	public static String TOPICTYPE_SM = "./topictype_sm.gif";
-	public static String ASSOCIATIONTYPE_SM = "./associationtype_sm.gif";
+	public static String OCCURRENCETYPE_BW = "./occurrencetype_bw.gif";
 	public static String OCCURRENCETYPE_SM = "./occurrencetype_sm.gif";
-	public static String TOPICTYPE = "./topictype.gif";
-	public static String TOPIC = "./topic.gif";
-	public static String TOPIC_SM = "./topic.gif";
-	public static String ASSOCIATIONTYPE = "./associationtype.gif";
 	public static String ROLETYPE = "./roletype.gif";
-	public static String KINDOF = "./kindof.gif";
-	public static String NAMECONSTRAINT_SM = "./nameconstraint_sm.gif";
-	public static String ASSOCIATIONCONSTRAINT_SM = "./associationconstraint_sm.gif";
-	public static String COMMENT = "./comment.png";
+	public static String ROLETYPE_BW = "./roletype_bw.gif";
+	public static String ROLETYPE_SM = "./roletype_sm.gif";
+	public static String SUBJECTIDENTIFIERCONSTRAINT = "./siconstraint.gif";
+	public static String SUBJECTLOCATORCONSTRAINT = "./slconstraint.gif";
+	public static String TOPIC = "./topic.gif";
+	public static String TOPIC_BW = "./topic_bw.gif";
+	public static String TOPIC_SM = "./topic.gif";
 	public static String TOPICROLE = "./rp.gif";
 	public static String TOPICROLE_SM = "./rp_sm.gif";
-	public static String SUBJECTIDENTIFIERCONSTRAINT = "./siconstraint.gif";
-	public static String ITEMIDENTIFIERCONSTRAINT = "./iiconstraint.gif";
-	public static String SUBJECTLOCATORCONSTRAINT = "./slconstraint.gif";
-	public static String ICON = "./icon.png";
-	public static String DOMAINDIAGRAM = "./domaindiagram.gif";
-	public static String DIAGRAM = "./diagram.gif";
-	
+	public static String TOPICTYPE = "./topictype.gif";
+	public static String TOPICTYPE_BW = "./topictype_bw.gif";
+	public static String TOPICTYPE_SM = "./topictype_sm.gif";
 	public static String VALIDATE = "./icon.png";
+
 }
