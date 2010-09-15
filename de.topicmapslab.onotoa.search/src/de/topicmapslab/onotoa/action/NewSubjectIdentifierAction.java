6f0c0c42fd349341a32ac2dbe3b6c2a8c0ca1692
lippert <lippert@localhost>
2010-09-15 22:47:26 +0200

    Implemented undo/redo behaviour for searchView actions. Needs improvment

--- de.topicmapslab.onotoa.search/src/de/topicmapslab/onotoa/action/NewSubjectIdentifierAction.java
+++ de.topicmapslab.onotoa.search/src/de/topicmapslab/onotoa/action/NewSubjectIdentifierAction.java
@@ -13,6 +13,7 @@ package de.topicmapslab.onotoa.action;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.emf.common.command.CommandStack;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.IInputValidator;
@@ -34,15 +35,17 @@ public class NewSubjectIdentifierAction extends Action {
 
 	private TreeViewer viewer;
 	private TopicType topicType;
+	private CommandStack commandStack;
 
 	/**
 	 * @param string
 	 * @param viewer
 	 */
-	public NewSubjectIdentifierAction(String label, TreeViewer viewer) {
+	public NewSubjectIdentifierAction(CommandStack commandStack ,String label, TreeViewer viewer) {
 
 		super(label, Action.AS_PUSH_BUTTON);
 		this.viewer = viewer;
+		this.commandStack = commandStack;
 
 	}
 
@@ -72,7 +75,7 @@ public class NewSubjectIdentifierAction extends Action {
 			List<String> list = new ArrayList<String>();
 			list.add(result);
 			SetTopicTypeIdentifiersCommand command = new SetTopicTypeIdentifiersCommand(list, topicType);
-			command.execute();
+			commandStack.execute(command);
 
 		}
 
