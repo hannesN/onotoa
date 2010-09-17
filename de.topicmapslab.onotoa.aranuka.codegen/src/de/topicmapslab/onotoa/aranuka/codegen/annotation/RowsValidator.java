38b6e86de61fb00e3ede0032c6fe4f30d5457ce9
niederhausen <niederhausen@localhost>
2010-09-17 10:14:16 +0200

    bugfix and dependency update

--- de.topicmapslab.onotoa.aranuka.codegen/src/de/topicmapslab/onotoa/aranuka/codegen/annotation/RowsValidator.java
+++ de.topicmapslab.onotoa.aranuka.codegen/src/de/topicmapslab/onotoa/aranuka/codegen/annotation/RowsValidator.java
@@ -4,15 +4,15 @@
 package de.topicmapslab.onotoa.aranuka.codegen.annotation;
 
 /**
- * @author niederhausen
+ * @author Hannes Niederhausen
  * 
  */
 public class RowsValidator extends IntegerValidator {
 
 	@Override
-	public boolean isValid(Object obj) {
+	public boolean isValid(String obj) {
 		if (super.isValid(obj)) {
-			return (((Integer)obj) >= 1);
+			return Integer.parseInt(obj) >= 1;
 		}
 		return false;
 	}
