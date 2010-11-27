3f52d34d2081f96097f7cf2819cbe7ba0675da66
lippert <lippert@localhost>
2010-11-28 00:20:01 +0100

    Collateral damage of a rename action

--- de.topicmapslab.onotoa.search/src/de/topicmapslab/onotoa/search/util/UseFinder.java
+++ de.topicmapslab.onotoa.search/src/de/topicmapslab/onotoa/search/util/UseFinder.java
@@ -16,7 +16,6 @@ import java.util.List;
 import org.eclipse.core.runtime.IProgressMonitor;
 
 import de.topicmapslab.onotoa.search.model.tree.TreeNode;
-import de.topicmapslab.onotoa.search.model.tree.TreeNodeType;
 import de.topicmapslab.tmcledit.model.AssociationType;
 import de.topicmapslab.tmcledit.model.AssociationTypeConstraint;
 import de.topicmapslab.tmcledit.model.NameTypeConstraint;
@@ -71,7 +70,7 @@ public class UseFinder {
 
 		for (AssociationTypeConstraint atc : schema.getAssociationTypeConstraints()) {
 			if (atc.getType().equals(type))
-				result.add(new TreeNode(atc, TreeNodeType.Association));
+				result.add(new TreeNode(atc, UseType.Association));
 		}
 
 		monitor.done();
@@ -84,38 +83,38 @@ public class UseFinder {
 	 * @return
 	 */
 	private static TreeNode getUse(TopicType type, TopicType tt) {
-		TreeNode tn = new TreeNode(tt, TreeNodeType.User);
+		TreeNode tn = new TreeNode(tt, UseType.User);
 		for (TopicType isa : tt.getIsa()) {
 			if (isa.equals(type))
-				tn.addChild(new TreeNode(isa, TreeNodeType.Type));
+				tn.addChild(new TreeNode(isa, UseType.Type));
 		}
 
 		for (TopicType ako : tt.getAko()) {
 			if (ako.equals(type))
-				tn.addChild(new TreeNode(ako, TreeNodeType.Supertype));
+				tn.addChild(new TreeNode(ako, UseType.Supertype));
 		}
 
 		for (NameTypeConstraint ntc : tt.getNameConstraints()) {
 			if (type.equals(ntc.getType())) {
-				tn.addChild(new TreeNode(ntc, TreeNodeType.Nametype));
+				tn.addChild(new TreeNode(ntc, UseType.Nametype));
 			}
 		}
 
 		for (OccurrenceTypeConstraint otc : tt.getOccurrenceConstraints()) {
 			if (type.equals(otc.getType())) {
-				tn.addChild(new TreeNode(otc, TreeNodeType.OccurrenceType));
+				tn.addChild(new TreeNode(otc, UseType.OccurrenceType));
 			}
 		}
 
 		if (tt instanceof AssociationType) {
 			for (RolePlayerConstraint rpc : ModelIndexer.getAssociationIndexer().getRolePlayerConstraintsFor(tt)) {
 				if (rpc.getPlayer().equals(type))
-					tn.addChild(new TreeNode(rpc, TreeNodeType.Player));
+					tn.addChild(new TreeNode(rpc, UseType.Player));
 			}
 
 			for (RoleConstraint rc : ((AssociationType) tt).getRoles()) {
 				if (rc.getType().equals(type)) {
-					tn.addChild(new TreeNode(rc, TreeNodeType.Role));
+					tn.addChild(new TreeNode(rc, UseType.Role));
 				}
 			}
 		}
