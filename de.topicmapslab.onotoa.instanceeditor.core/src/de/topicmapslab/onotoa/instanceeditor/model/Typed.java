a2ab44ebc5662b9833b1117c87ba4d5b0a20158a
hass <hass@localhost>
2011-02-15 13:06:52 +0100

    implement names, occurrences and characteristics class

--- de.topicmapslab.onotoa.instanceeditor.core/src/de/topicmapslab/onotoa/instanceeditor/model/Typed.java
+++ de.topicmapslab.onotoa.instanceeditor.core/src/de/topicmapslab/onotoa/instanceeditor/model/Typed.java
@@ -2,9 +2,22 @@ package de.topicmapslab.onotoa.instanceeditor.model;
 
 import de.topicmapslab.onotoa.instanceeditor.tmql.StatementProvider;
 
+/**
+ * abstract class for typed constructs
+ * @author Christian Haß
+ *
+ */
 public abstract class Typed extends Construct {
 
+	/**
+	 * constructor
+	 * @param id - the construct id
+	 * @param statementProvider - the statement provider
+	 */
 	public Typed(String id, StatementProvider statementProvider) {
 		super(id, statementProvider);
 	}
+	
+	
+	
 }
