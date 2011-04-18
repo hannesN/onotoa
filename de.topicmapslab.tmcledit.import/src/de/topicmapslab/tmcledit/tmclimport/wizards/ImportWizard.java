b443433e0fbccca4d956c10455bee2c7ca1a8218
lippert <lippert@localhost>
2011-04-18 16:07:17 +0200

    Add error dialog for import errors

--- de.topicmapslab.tmcledit.import/src/de/topicmapslab/tmcledit/tmclimport/wizards/ImportWizard.java
+++ de.topicmapslab.tmcledit.import/src/de/topicmapslab/tmcledit/tmclimport/wizards/ImportWizard.java
@@ -13,6 +13,7 @@ package de.topicmapslab.tmcledit.tmclimport.wizards;
 import java.lang.reflect.InvocationTargetException;
 
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.dialogs.ProgressMonitorDialog;
 import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.viewers.IStructuredSelection;
@@ -29,7 +30,7 @@ import de.topicmapslab.tmcledit.tmclimport.Activator;
 import de.topicmapslab.tmcledit.tmclimport.builder.OnotoaBuilder;
 
 public class ImportWizard extends Wizard implements IImportWizard {
-	
+
 	ImportWizardPage mainPage;
 
 	public ImportWizard() {
@@ -37,115 +38,114 @@ public class ImportWizard extends Wizard implements IImportWizard {
 	}
 
 	public boolean performFinish() {
-		
-		
+
 		ProgressMonitorDialog monitorDlg = new ProgressMonitorDialog(getShell());
 		monitorDlg.setCancelable(false);
-		
+
 		try {
-	        monitorDlg.run(false, false, new LoaderRunnable());
-        } catch (InvocationTargetException e) {
-	        // TODO Auto-generated catch block
-	        e.printStackTrace();
-        } catch (InterruptedException e) {
-	        // TODO Auto-generated catch block
-	        e.printStackTrace();
-        }
-		
-//		File file = builder.getFile();
-//
-//		if (file.getFilename()!=null)
-//			Activator.getDefault().getPreferenceStore().putValue("last_imported_file", builder.getFile().getFilename());
-//		
-//		try {
-//			IWorkbenchPage activePage = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
-//			ModelView mv = (ModelView) activePage.findView(ModelView.ID);
-//			if (mv==null) {
-//				// TODO look at msg
-//				System.out.println("Error, please show a dialog");
-//			}
-//			
-//			mv.setFilename("", true, file);
-//			
-//			
-//			activePage.activate(mv);
-//			
-//		} catch (Exception e) {
-//			throw new RuntimeException(e);
-//		}
-		
+			monitorDlg.run(false, false, new LoaderRunnable());
+		} catch (Exception e) {
+			Activator.logException(e);
+			MessageDialog.openError(getShell(), "Error while importing schema", "An error occurred:" + e.getMessage()
+			        + "[" + e.getClass().getName() + "]");
+		}
+
+		// File file = builder.getFile();
+		//
+		// if (file.getFilename()!=null)
+		// Activator.getDefault().getPreferenceStore().putValue("last_imported_file",
+		// builder.getFile().getFilename());
+		//
+		// try {
+		// IWorkbenchPage activePage =
+		// PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+		// ModelView mv = (ModelView) activePage.findView(ModelView.ID);
+		// if (mv==null) {
+		// // TODO look at msg
+		// System.out.println("Error, please show a dialog");
+		// }
+		//
+		// mv.setFilename("", true, file);
+		//
+		//
+		// activePage.activate(mv);
+		//
+		// } catch (Exception e) {
+		// throw new RuntimeException(e);
+		// }
+
 		return true;
 	}
-	 
-	public void init(IWorkbench workbench, IStructuredSelection selection) {
-		setWindowTitle("TMCL Schema Import Wizard"); //NON-NLS-1
-		setNeedsProgressMonitor(true);
-		mainPage = new ImportWizardPage("Import TMCL schema",selection); //NON-NLS-1
-	}
-	
-	/* (non-Javadoc)
-     * @see org.eclipse.jface.wizard.IWizard#addPages()
-     */
-    public void addPages() {
-        super.addPages(); 
-        addPage(mainPage);        
-    }
 
-    private class LoaderRunnable implements IRunnableWithProgress, IWorkMonitor {
-    	private IProgressMonitor monitor;
-    	private boolean started;
-    	private int steps;
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		setWindowTitle("TMCL Schema Import Wizard"); // NON-NLS-1
+		setNeedsProgressMonitor(true);
+		mainPage = new ImportWizardPage("Import TMCL schema", selection); // NON-NLS-1
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.jface.wizard.IWizard#addPages()
+	 */
+	public void addPages() {
+		super.addPages();
+		addPage(mainPage);
+	}
+
+	private class LoaderRunnable implements IRunnableWithProgress, IWorkMonitor {
+		private IProgressMonitor monitor;
+		private boolean started;
+		private int steps;
 		private int numberOfSteps;
-    	
+
 		public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
-	        this.monitor = monitor;
-	        
-	        OnotoaBuilder builder = new OnotoaBuilder(mainPage.getFile());
+			this.monitor = monitor;
+
+			OnotoaBuilder builder = new OnotoaBuilder(mainPage.getFile());
 			File file = builder.getFile(this);
 
-			if (file.getFilename()!=null)
+			if (file.getFilename() != null)
 				Activator.getDefault().getPreferenceStore().putValue("last_imported_file", file.getFilename());
-			
+
 			try {
 				IWorkbenchPage activePage = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
 				ModelView mv = (ModelView) activePage.findView(ModelView.ID);
-				if (mv==null) {
+				if (mv == null) {
 					// TODO look at msg
 					System.out.println("Error, please show a dialog");
 				}
-				
+
 				mv.setFilename("", true, file);
-				
-				
+
 				activePage.activate(mv);
-				
+
 			} catch (Exception e) {
 				throw new RuntimeException(e);
 			}
-			
-	        
-        }
+
+		}
 
 		public void finished() {
 			monitor.worked(numberOfSteps);
-        }
+		}
 
 		public void start(int numberOfSteps) {
-	        this.monitor.beginTask("Importing schema...", numberOfSteps);
-	        this.started = true;
-	        this.steps = 0;
-	        this.numberOfSteps = numberOfSteps;
-        }
+			this.monitor.beginTask("Importing schema...", numberOfSteps);
+			this.started = true;
+			this.steps = 0;
+			this.numberOfSteps = numberOfSteps;
+		}
 
 		public boolean isStarted() {
-	        return started;
-        }
+			return started;
+		}
 
 		public void stepDone() {
-	        steps++;
-	        monitor.worked(steps);
-        }
-    	
-    }
-    
+			steps++;
+			monitor.worked(steps);
+		}
+
+	}
+
 }
